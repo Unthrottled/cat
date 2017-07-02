778ec96653de5fae78ab009b0959bce87d9f0d23
cyclic-reference <alex@cyclicreference.space>
2017-07-02 18:53:27 -0500

    Counting sort was ez

--- src/main/java/io/acari/CountSort.java
+++ src/main/java/io/acari/CountSort.java
@@ -3,6 +3,20 @@ package io.acari;
 public class CountSort {
 
     public int[] sort(int array[]){
-        return array;
+        int[] counto = new int[1000];
+        for (int anArray : array) {
+            counto[anArray]++;
+        }
+        for(int i = 1; i < counto.length; i++){
+            counto[i] += counto[i - 1];
+        }
+
+        int[] returnArray = new int[array.length];
+        for (int guy : array) {
+            returnArray[counto[guy] - 1] = guy;
+            counto[guy]--;
+        }
+
+        return returnArray;
     }
 }
