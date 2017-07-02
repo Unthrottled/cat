a10ae064767e6c619dda72ab6cb85090d14e5c05
cyclic-reference <alex@cyclicreference.space>
2017-07-02 11:55:15 -0500

    tests and impl

--- src/main/java/io/acari/binary/QuickSort.java
+++ src/main/java/io/acari/binary/QuickSort.java
@@ -3,6 +3,34 @@ package io.acari.binary;
 public class QuickSort {
 
     public int[] sort(int array[]){
+        _quickSort(array, 0, array.length -1);
         return array;
+    }
+
+    private void _quickSort(int[] array, int low, int high) {
+        if(low < high){
+            int pivot = _partition(array, low, high);
+            _quickSort(array, low, pivot - 1);
+            _quickSort(array, ++pivot, high);
+        }
+    }
+
+    private int _partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++){
+            if(array[j] <= pivot){
+                _swap(array, ++i, j);
+            }
+        }
+        _swap(array, ++i, high);
+
+        return i;
+    }
+
+    private void _swap(int[] array, int i, int pivot) {
+        int tempo = array[i];
+        array[i] = array[pivot];
+        array[pivot] = tempo;
     }
 }
--- src/test/java/io/acari/binary/QuickSortTest.java
+++ src/test/java/io/acari/binary/QuickSortTest.java
@@ -9,34 +9,46 @@ import static org.junit.Assert.*;
  */
 public class QuickSortTest {
     private final QuickSort steve = new QuickSort();
+
     @Test
     public void sortShouldSortParta() throws Exception {
         int[] input = {1, 2, 3};
         int[] expected = {1, 2, 3};
         assertArrayEquals(expected, steve.sort(input));
     }
+
     @Test
     public void sortShouldSortPartb() throws Exception {
-        int[] input = {3,2,1};
+        int[] input = {3, 2, 1};
         int[] expected = {1, 2, 3};
         assertArrayEquals(expected, steve.sort(input));
     }
+
     @Test
     public void sortShouldSortPartc() throws Exception {
-        int[] input = {3,1,2};
+        int[] input = {3, 1, 2};
         int[] expected = {1, 2, 3};
         assertArrayEquals(expected, steve.sort(input));
     }
+
     @Test
     public void sortShouldSortPartd() throws Exception {
-        int[] input = {1,3,2};
+        int[] input = {1, 3, 2};
         int[] expected = {1, 2, 3};
         assertArrayEquals(expected, steve.sort(input));
     }
+
     @Test
     public void sortShouldSortParte() throws Exception {
-        int[] input = {36,8,13,5,21,1,3,2};
-        int[] expected = {1,2,3,5,8,13,21,36};
+        int[] input = {36, 8, 13, 5, 21, 1, 3, 2};
+        int[] expected = {1, 2, 3, 5, 8, 13, 21, 36};
+        assertArrayEquals(expected, steve.sort(input));
+    }
+
+    @Test
+    public void sortShouldSortPartf() throws Exception {
+        int[] input = {10, 80, 30, 90, 40, 50, 70};
+        int[] expected = {10, 30, 40, 50, 70, 80, 90};
         assertArrayEquals(expected, steve.sort(input));
     }
 }
