037a49b364a4003cb604aa2bf670e9ae9621926c
cyclic-reference <alex@cyclicreference.space>
2017-07-01 20:47:56 -0500

    Gunna do quick sort

---
+++ src/main/java/io/acari/binary/QuickSort.java
@@ -0,0 +1,8 @@
+package io.acari.binary;
+
+public class QuickSort {
+
+    public int[] sort(int array[]){
+        return array;
+    }
+}
---
+++ src/test/java/io/acari/binary/QuickSortTest.java
@@ -0,0 +1,15 @@
+package io.acari.binary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class QuickSortTest {
+    @Test
+    public void sort() throws Exception {
+    }
+
+}
