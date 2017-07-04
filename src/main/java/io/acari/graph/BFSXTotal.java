263fd573668c0b3d14ad5e2989fbf799f02d50eb
cyclic-reference <alex@cyclicreference.space>
2017-07-04 06:52:17 -0500

    GFG codez

--- src/main/java/io/acari/graph/BFSXTotal.java
+++ src/main/java/io/acari/graph/BFSXTotal.java
@@ -3,6 +3,9 @@ package io.acari.graph;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.stream.Stream;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class BFSXTotal {
 
@@ -65,6 +68,33 @@ public class BFSXTotal {
                     new Node(x - 1, y),//left
                     new Node(x + 1, y)//right
             );
+        }
+    }
+}
+
+
+class GFG
+{
+    public static void main (String[] args)
+    {
+        BFSXTotal steve = new BFSXTotal();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        in.nextLine();
+        for(int i_t = 0; i_t < t; i_t++){
+            int height = in.nextInt();
+            int width = in.nextInt();
+            char[][] matrix = new char[height][width];
+            in.nextLine();
+            String line = in.nextLine();
+            String[] rows = line.split(" ");
+            for(int i = 0; i < height; i++){
+                String rowLine = rows[i];
+                for(int j=0; j < width; j++){
+                    matrix[i][j] = rowLine.charAt(j);
+                }
+            }
+            System.out.println(steve.findTree(matrix));
         }
     }
 }
