d4ad8857a1ae1c5c41509b35dd599ea232ad9bb6
cyclic-reference <alex@cyclicreference.space>
2017-07-08 16:42:06 -0500

    things for l8r

---
+++ src/main/java/io/acari/graph/GFG2.java
@@ -0,0 +1,44 @@
+package io.acari.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+class GFG2 {
+    public static void main(String[] args) {
+        RootToLeaf steve = new RootToLeaf();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        in.nextLine();
+        for (int i_t = 0; i_t < t; i_t++) {
+            int nodes = in.nextInt();
+            Map<Integer, Node> graph = new HashMap<>(nodes);
+            in.nextLine();
+            String line = in.nextLine();
+            String[] rows = line.split(" ");
+            for (int i = 0; i < rows.length - 2; i += 3) {
+                int root = Integer.parseInt(rows[i]);
+                int child = Integer.parseInt(rows[i + 1]);
+                String side = rows[i + 2];
+                Node rootTreeNode = graph.getOrDefault(root, new Node(root));
+                Node childTreeNode = graph.getOrDefault(child, new Node(child));
+                if (childTreeNode.left != null && childTreeNode.right != null) {
+                    childTreeNode = new Node(child);
+                }
+
+                if (side.charAt(0) == 'L' && rootTreeNode.left == null) {
+                    rootTreeNode.left = childTreeNode;
+                } else if (rootTreeNode.right == null) {
+                    rootTreeNode.right = childTreeNode;
+                }
+
+                graph.put(rootTreeNode.data, rootTreeNode);
+                graph.put(childTreeNode.data, childTreeNode);
+            }
+            steve.rootToLeaf(graph.get(parseInt(rows[0])));
+            System.out.println();
+        }
+    }
+}
