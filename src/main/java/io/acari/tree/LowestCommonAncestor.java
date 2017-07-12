830d03f656694fb7431a8a24b0a7249bc386e2f4
cyclic-reference <alex@cyclicreference.space>
2017-07-11 20:10:03 -0500

    Solved eet

--- src/main/java/io/acari/tree/LowestCommonAncestor.java
+++ src/main/java/io/acari/tree/LowestCommonAncestor.java
@@ -2,22 +2,17 @@ package io.acari.tree;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class LowestCommonAncestor {
 
     public int lowestCommonAncestor(Node root, Node one, Node two) {
         if (root != null) {
-            Deque<Node> onePath = new LinkedList<>();
-            doWork(root, one, onePath);
+            Deque<Node> onePath = doWork(root, one);
             if(onePath.isEmpty()){
                 return -1;
             }
-            Deque<Node> twoPath = new LinkedList<>();
-            doWork(root, two, twoPath);
+            Deque<Node> twoPath = doWork(root, two);
             if(twoPath.isEmpty()){
                 return -1;
             }
@@ -40,30 +35,35 @@ public class LowestCommonAncestor {
         return -1;
     }
 
-    private void doWork(Node root, Node one, Deque<Node> onePath) {
-        Deque<Node> deque = new LinkedList<>();
-        deque.offer(root);
-        while (!deque.isEmpty()) {
-            Node currentNode = deque.poll();
-            if (currentNode.data == one.data) {
-                onePath.offer(currentNode);
-                break;
+    private Deque<Node> doWork(Node root, Node nodeToFind) {
+        if(root != null){
+            if(root.data == nodeToFind.data){
+                Deque<Node> deque = new LinkedList<>();
+                deque.push(root);
+                return deque;
             }
 
-            if (currentNode.left == null && currentNode.right == null) {
-                onePath.pollLast();
-            } else {
-                onePath.offer(currentNode);
-                if (currentNode.right != null) {
-                    deque.push(currentNode.right);
-                }
-
-                if (currentNode.left != null) {
-                    deque.push(currentNode.left);
-                }
+            Node right = root.right;
+            if(right != null){
+                Deque<Node> deque = getNodes(nodeToFind, right);
+                if (deque != null) return deque;
             }
-
+            Node left = root.left;
+            if (left != null){
+                Deque<Node> deque = getNodes(nodeToFind, left);
+                if (deque != null) return deque;
+            }
         }
+        return new ArrayDeque<>();
+    }
+
+    private Deque<Node> getNodes(Node one, Node right) {
+        Deque<Node> deque = doWork(right, one);
+        if(!deque.isEmpty()){
+            deque.push(right);
+            return deque;
+        }
+        return null;
     }
 
     public void doThing(Node node) {
