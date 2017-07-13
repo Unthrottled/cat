23d784d05731f453606c410f54f2e31979852a03
cyclic-reference <alex@cyclicreference.space>
2017-07-13 14:45:39 -0500

    gonna do this

---
+++ src/main/java/io/acari/tree/DeepestLeftNode.java
@@ -0,0 +1,15 @@
+package io.acari.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DeepestLeftNode {
+
+    public Node find(Node root){
+        if(root != null){
+
+        }
+
+        return root;
+    }
+}
