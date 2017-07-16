88f8fd1b34790d68f65888d1dd49b2ac36d99a7a
cyclic-reference <alex@cyclicreference.space>
2017-07-15 21:14:47 -0500

    dis is a wip

--- src/main/java/io/acari/tree/DeepestLeftNode.java
+++ src/main/java/io/acari/tree/DeepestLeftNode.java
@@ -6,9 +6,34 @@ public class DeepestLeftNode {
 
     public Node find(Node root) {
         if (root != null) {
+            return rootUtl(root, 0, 0);
+        }
+
+        return root;
+    }
+
+    public Node rootUtl(Node root, int max, int currentLevel){
+        if(root != null){
+            Node rightChild = root.right;
+            currentLevel++;
+            Node lowestLeftRight = rootUtl(rightChild, max, currentLevel);
+            Node leftChild = root.left;
+            if(leftChild.left == null && leftChild.right == null){
+                if(currentLevel > max){
+                    return leftChild;
+                } else {
+                 Node lowestLeftLeft = rootUtl(leftChild, max, currentLevel);
+
+                }
+            } else {
+
+                return rightChild;
+            }
+
 
         }
 
         return root;
+
     }
 }
