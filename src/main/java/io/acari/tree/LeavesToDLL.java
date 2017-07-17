3d6175536c6c421c2993f355db5a2964490b1e96
cyclic-reference <alex@cyclicreference.space>
2017-07-16 19:40:50 -0500

    Was gonna do dis but got a c++ only problem :|

---
+++ src/main/java/io/acari/tree/LeavesToDLL.java
@@ -0,0 +1,11 @@
+package io.acari.tree;
+
+import io.acari.tree.pojo.Node;
+
+public class LeavesToDLL {
+
+    //SOVLED IN C++ :|
+    public Node convertToDLL(Node root){
+        return null;
+    }
+}
