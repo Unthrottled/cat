62ff2d8069ba08fe3c6c45cc200b859f86e15396
cyclic-reference <alex@cyclicreference.space>
2017-07-19 16:31:11 -0500

    TOTALLY TABULAR

--- src/main/java/io/acari/dp/LongestCommonSequence.java
+++ src/main/java/io/acari/dp/LongestCommonSequence.java
@@ -5,14 +5,16 @@ import java.util.Arrays;
 public class LongestCommonSequence {
     private static int ayy = 0;
     private static int lemon = 0;
+    private static int its = 0;
 
     public static void main(String... args) {
         LongestCommonSequence steve = new LongestCommonSequence();
         System.out.println(steve.find("nematode knowledge", "empty bottle"));
         System.out.println(steve.betterFind("nematode knowledge", "empty bottle"));
+        System.out.println(steve.evenBetterFind("nematode knowledge", "empty bottle"));
 
 
-        System.out.println(ayy + " " + lemon);
+        System.out.println(ayy + " " + lemon + " " + its);
     }
 
     public int find(String one, String two) {
@@ -33,6 +35,24 @@ public class LongestCommonSequence {
         }
 
         return betterFindUtil(one, two, 0, 0, lookup);
+    }
+
+    public int evenBetterFind(String one, String two){
+        int[][] lookup = new int[one.length() + 1][two.length() + 1];
+        for(int i = one.length(); i >= 0; i--){
+            for(int j = two.length(); j >= 0; j--){
+                its++;
+                if(i >= one.length() || j >= two.length()){
+                    lookup[i][j] = 0;
+                } else if (one.charAt(i) == two.charAt(j)){
+                    lookup[i][j] = 1 + lookup[i + 1][j + 1];
+                } else {
+                    lookup[i][j] = Math.max(lookup[i + 1][j], lookup[i][j + 1]);
+                }
+            }
+        }
+
+        return lookup[0][0];
     }
 
     private int betterFindUtil(String one, String two, int i, int j, int[][] lookup) {
