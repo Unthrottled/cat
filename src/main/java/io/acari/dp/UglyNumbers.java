7bf43a471a38a6c77a316c630ba485bab13b6c76
cyclic-reference <alex@cyclicreference.space>
2017-07-20 13:37:44 -0500

    New Crass

---
+++ src/main/java/io/acari/dp/UglyNumbers.java
@@ -0,0 +1,14 @@
+package io.acari.dp;
+
+import java.util.Scanner;
+
+public class UglyNumbers {
+
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for(int i_t = 0; i_t < testCases; i_t++){
+
+        }
+    }
+}
