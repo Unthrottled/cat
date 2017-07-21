3517fa5a3c362f4d1a684842b9cbd58c767d87b4
cyclic-reference <alex@cyclicreference.space>
2017-07-21 15:34:12 -0500

    Cumpleted, thanx gfg

--- src/main/java/io/acari/dp/LongestIncreasingSequence.java
+++ src/main/java/io/acari/dp/LongestIncreasingSequence.java
@@ -1,10 +1,22 @@
 package io.acari.dp;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class LongestIncreasingSequence {
 
-    public long find(List<Integer> guy){
-        return guy.size();
+    public int find(List<Integer> guy){
+        int[] lis = new int[guy.size()];
+        Arrays.fill(lis, 1);
+        for(int i = 1; i < lis.length; i++){
+            for(int j = 0; j < i; j++){
+                int left = guy.get(j);
+                int right = guy.get(i);
+                if(left < right){
+                    lis[i] = Math.max(lis[j] + 1, lis[i]);
+                }
+            }
+        }
+        return Arrays.stream(lis).max().orElse(1);
     }
 }
