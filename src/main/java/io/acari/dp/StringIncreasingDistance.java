918cffcd5e3cd5038ff9af5ec9d6ff7010e1026f
cyclic-reference <alex@cyclicreference.space>
2017-07-23 18:47:56 -0500

    yay

--- src/main/java/io/acari/dp/StringIncreasingDistance.java
+++ src/main/java/io/acari/dp/StringIncreasingDistance.java
@@ -2,7 +2,32 @@ package io.acari.dp;
 
 public class StringIncreasingDistance {
 
-    public int find(String string){
+    public int find(String string) {
+        int length = string.length();
+        if (length > 2) {
+            for (int j = length - 1; j > -1; --j) {
+                if (reachesEnd(string.substring(0, j), string.charAt(j))) {
+                    return j + 1;
+                }
+            }
+        }
         return -1;
+    }
+
+
+    private boolean reachesEnd(String str, char guy) {
+        if (!str.isEmpty()) {
+            if (str.charAt(0) == guy) {
+                return true;
+            } else {
+                for (int i = str.length() - 1; i > -1; i--) {
+                    char otherGuy = str.charAt(i);
+                    if (otherGuy + 1 == guy) {
+                        return reachesEnd(str.substring(0, i + 1), otherGuy);
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
