91dbdfc538563261218478dd5e8620e2c8e9bea6
cyclic-reference <alex@cyclicreference.space>
2017-07-23 16:11:57 -0500

    I think i thought i did a do?

--- src/main/java/io/acari/dp/LongestCommonSubsequence.java
+++ src/main/java/io/acari/dp/LongestCommonSubsequence.java
@@ -3,6 +3,16 @@ package io.acari.dp;
 public class LongestCommonSubsequence {
 
     public int find(String one, String two){
-        return -1;
+        int oneLength = one.length();
+        int twoLength = two.length();
+        int[][] matrix = new int[oneLength +1][twoLength + 1];
+        for(int i = oneLength; i > -1; i--){
+            for(int j = twoLength; j > -1; j--){
+                if(i >= oneLength || j >= twoLength) matrix[i][j] = 0;
+                else if(one.charAt(i) == two.charAt(j)) matrix[i][j] = 1 + matrix[i + 1][j + 1];
+                else matrix[i][j] = Math.max(matrix[i][j + 1], matrix[i + 1][j]);
+            }
+        }
+        return matrix[0][0];
     }
 }
