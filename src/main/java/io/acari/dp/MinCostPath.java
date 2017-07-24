18d841217edbc8db80be0a833caa236a050a6bf8
cyclic-reference <alex@cyclicreference.space>
2017-07-23 19:14:04 -0500

    driver

--- src/main/java/io/acari/dp/MinCostPath.java
+++ src/main/java/io/acari/dp/MinCostPath.java
@@ -5,4 +5,10 @@ public class MinCostPath {
     public int find(int[][] costs){
         return -1;
     }
+
+    public static void main(String... args){
+        MinCostPath minCostPath = new MinCostPath();
+        int[][] b = {{1,2,3},{4,8,2},{1,5,3}};
+        System.out.println(minCostPath.find(b));
+    }
 }
