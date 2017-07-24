4cc5c2f6470fd56e07ac373831ee1834a92230d0
cyclic-reference <alex@cyclicreference.space>
2017-07-24 15:18:27 -0500

    Driver, and I need to learn how to read

--- src/main/java/io/acari/dp/LongestSubstringNoRepeat.java
+++ src/main/java/io/acari/dp/LongestSubstringNoRepeat.java
@@ -2,7 +2,7 @@ package io.acari.dp;
 
 public class LongestSubstringNoRepeat {
 
-    public int find(String one, String two){
+    public int find(String one){
         return -1;
     }
 }
--- src/main/java/io/acari/dp/driver/GFG7.java
+++ src/main/java/io/acari/dp/driver/GFG7.java
@@ -1,13 +1,14 @@
 package io.acari.dp.driver;
 
 import io.acari.dp.LongestCommonSubsequence;
+import io.acari.dp.LongestSubstringNoRepeat;
 import io.acari.dp.StringIncreasingDistance;
 
 import java.util.Scanner;
 
 public class GFG7 {
     public static void main(String[] args) {
-        StringIncreasingDistance steve = new StringIncreasingDistance();
+        LongestSubstringNoRepeat steve = new LongestSubstringNoRepeat();
         Scanner in = new Scanner(System.in);
         int t = Integer.parseInt(in.nextLine());
         for (int i_t = 0; i_t < t; i_t++) {
