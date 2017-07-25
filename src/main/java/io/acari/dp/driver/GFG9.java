bbfb774a251415ee7d6dc4591fdbc0fd553201ca
cyclic-reference <alex@cyclicreference.space>
2017-07-25 15:52:29 -0500

    driver

---
+++ src/main/java/io/acari/dp/driver/GFG9.java
@@ -0,0 +1,23 @@
+package io.acari.dp.driver;
+
+import io.acari.dp.MinJumps;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class GFG9 {
+
+    public static void main(String... args) {
+        MinJumps steve = new MinJumps();
+        Scanner in = new Scanner(System.in);
+        int t = Integer.parseInt(in.nextLine());
+        for (int i_t = 0; i_t < t; i_t++) {
+            String num = in.nextLine();
+            String line = in.nextLine();
+            Integer[] coinz = new Integer[Integer.parseInt(num)];
+            coinz = Arrays.stream(line.split(" ")).map(Integer::parseInt).collect(Collectors.toList()).toArray(coinz);
+            System.out.println(steve.find(coinz));
+        }
+    }
+}
