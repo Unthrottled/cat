55dfac11d6dd716a25f4e6ac1a669001c8f3022c
cyclic-reference <alex@cyclicreference.space>
2017-07-27 16:27:40 -0500

    Driver

---
+++ src/main/java/io/acari/dp/driver/GFG12.java
@@ -0,0 +1,18 @@
+package io.acari.dp.driver;
+
+import io.acari.dp.NCR;
+
+import java.util.Scanner;
+
+public class GFG12 {
+
+    public static void main(String[] args) {
+        NCR steve = new NCR();
+        Scanner in = new Scanner(System.in);
+        int t = Integer.parseInt(in.nextLine());
+        for (int i_t = 0; i_t < t; i_t++) {
+            String[] num = in.nextLine().split(" ");
+            System.out.println(steve.find(Integer.parseInt(num[0]), Integer.parseInt(num[1])));
+        }
+    }
+}
