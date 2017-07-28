fdffdae1ee46bd88cd5f2f687b2090a8a607ea5b
cyclic-reference <alex@cyclicreference.space>
2017-07-28 06:46:56 -0500

    api

--- src/main/java/io/acari/string/PhoneNumberWords.java
+++ src/main/java/io/acari/string/PhoneNumberWords.java
@@ -1,4 +1,10 @@
 package io.acari.string;
 
+import java.util.stream.Stream;
+
 public class PhoneNumberWords {
+
+    public Stream<String> find(String[] numbers){
+        return Stream.empty();
+    }
 }
