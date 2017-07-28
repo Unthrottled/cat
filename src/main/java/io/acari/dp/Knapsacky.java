4988df6e1d0acc6b742418c05d15b8166cf3cd1c
cyclic-reference <alex@cyclicreference.space>
2017-07-28 11:27:16 -0500

    api

--- src/main/java/io/acari/dp/Knapsacky.java
+++ src/main/java/io/acari/dp/Knapsacky.java
@@ -1,4 +1,12 @@
 package io.acari.dp;
 
+import io.acari.dp.pojo.Item;
+
+import java.util.List;
+
 public class Knapsacky {
+
+    public int find(int sackCapacity, List<Item> items){
+        return -1;
+    }
 }
