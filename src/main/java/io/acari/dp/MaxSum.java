5956e31257a249b168410ce6cc9f79fae59f9855
cyclic-reference <alex@cyclicreference.space>
2017-07-30 18:34:20 -0500

    I deed eet

--- src/main/java/io/acari/dp/MaxSum.java
+++ src/main/java/io/acari/dp/MaxSum.java
@@ -1,8 +1,29 @@
 package io.acari.dp;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MaxSum {
 
-    public int find(Integer[] integers){
-        return -1;
+    public int find(Integer[] integers) {
+        Integer[] lookup = Arrays.copyOf(integers, integers.length);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < integers.length; i++) {
+            int subMax = 0;
+            for (int j = i - 1; j > -1; j--) {
+                int one = integers[i];
+                int two = integers[j];
+                if (one > two) {
+                    if (lookup[j] > subMax) subMax = lookup[j];
+                }
+            }
+
+            lookup[i] = Math.max(lookup[i], lookup[i] + subMax);
+
+            if (lookup[i] > max) max = lookup[i];
+
+        }
+
+        return max;
     }
 }
