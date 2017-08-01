243a4837bcefd3bb4afbc8842a6376e2616357e5
cyclic-reference <alex@cyclicreference.space>
2017-08-01 13:24:14 -0500

    PPDriver

--- src/main/java/io/acari/dp/driver/GFG11.java
+++ src/main/java/io/acari/dp/driver/GFG11.java
@@ -1,16 +1,13 @@
 package io.acari.dp.driver;
 
-import io.acari.dp.LongestPalindrome;
-import io.acari.dp.MinJumps;
+import io.acari.dp.PalindromicPartitioning;
 
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class GFG11 {
 
     public static void main(String... args) {
-        LongestPalindrome steve = new LongestPalindrome();
+        PalindromicPartitioning steve = new PalindromicPartitioning();
         Scanner in = new Scanner(System.in);
         int t = Integer.parseInt(in.nextLine());
         for (int i_t = 0; i_t < t; i_t++) {
