61ed8a6e3de43cace7722d7917dd33e69149b7fc
cyclic-reference <alex@cyclicreference.space>
2017-08-01 11:45:46 -0500

    FW Driver

---
+++ src/main/java/io/acari/dp/driver/GFG16.java
@@ -0,0 +1,31 @@
+package io.acari.dp.driver;
+
+import io.acari.dp.FloydWarshall;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class GFG16 {
+
+    public static void main(String[] args) {
+        FloydWarshall steve = new FloydWarshall();
+        Scanner in = new Scanner(System.in);
+        int t = Integer.parseInt(in.nextLine());
+        for (int i_t = 0; i_t < t; i_t++) {
+            String arguments = in.nextLine();
+            int rowEnd = Integer.parseInt(arguments);
+            String line = in.nextLine();
+            Integer[][] matrix = new Integer[rowEnd][rowEnd];
+            List<Integer> collect = Arrays.stream(line.trim().split(" ")).filter(s -> !s.isEmpty()).map(Integer::parseInt).collect(Collectors.toList());
+            int rollingGuy = 0;
+            for (int j = 0; j < rowEnd; j++) {
+                for (int i = 0; i < rowEnd; i++) {
+                    matrix[i][j] = collect.get(rollingGuy++);
+                }
+            }
+            System.out.println(steve.find(matrix));
+        }
+    }
+}
