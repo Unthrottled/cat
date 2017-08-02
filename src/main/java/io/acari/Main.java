b421b55ae1f1c393e337d8aaa9684ca362e09f34
cyclic-reference <alex@cyclicreference.space>
2017-08-01 19:32:15 -0500

    hmm

--- src/main/java/io/acari/Main.java
+++ src/main/java/io/acari/Main.java
@@ -2,4 +2,13 @@ package io.acari;
 
 public class Main {
 
+    public static void printArray(int[][] lookup) {
+        for (int[] ints : lookup) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/main/java/io/acari/dp/MatrixMulti.java
+++ src/main/java/io/acari/dp/MatrixMulti.java
@@ -1,29 +1,44 @@
 package io.acari.dp;
 
+
+import static io.acari.Main.printArray;
+
 public class MatrixMulti {
 
     public long find(Integer[] matrices) {
         int arrayLength = matrices.length;
         int[][] lookup = new int[arrayLength][arrayLength];
+        int[][] split = new int[arrayLength][arrayLength];
+        //Starts at 2 because you can't multiply on matrix
+        //Since [12, 10, 20] defines two matrices of 12 x 10 and 10 x 20
         for (int chainLength = 2; chainLength < arrayLength; chainLength++) {
             for (int i = 1; i < arrayLength - chainLength + 1; i++) {
                 int nextMatrix = chainLength + i - 1;
                 lookup[i][nextMatrix] = Integer.MAX_VALUE;
                 for (int k = i; k < nextMatrix; k++) {
-                    int dis = lookup[i][k] + lookup[k + 1][nextMatrix] + matrices[i - 1] * matrices[k] * matrices[nextMatrix];
-                    lookup[i][nextMatrix] = lookup[i][nextMatrix] > dis ? dis : lookup[i][nextMatrix];
+                    Integer column = matrices[i - 1];
+                    Integer row = matrices[nextMatrix];
+                    Integer otherColumn = matrices[k];
+                    int previousMin = lookup[i][k] + lookup[k + 1][nextMatrix];
+                    int currentCombiniation = previousMin + column * row * otherColumn ;
+                    int currentValue = lookup[i][nextMatrix];
+                    boolean b = currentValue > currentCombiniation;
+                    if(b){
+                        lookup[i][nextMatrix] = currentCombiniation;
+                        split[i][nextMatrix] = k;
+
+                    }
                 }
             }
         }
 
-        for (int[] ints : lookup) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
-            }
-            System.out.println();
-        }
+//        printArray(lookup);
+//        System.out.println();
+//        printArray(split);
 
 
         return lookup[1][arrayLength - 1];
     }
+
+
 }
