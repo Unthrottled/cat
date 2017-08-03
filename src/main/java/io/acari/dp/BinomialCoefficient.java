d97ce08c2c2bc6cc7f3c378cc5ecef00c4cdbc55
cyclic-reference <alex@cyclicreference.space>
2017-08-03 08:24:29 -0500

    Steel do not geet eet

--- src/main/java/io/acari/dp/BinomialCoefficient.java
+++ src/main/java/io/acari/dp/BinomialCoefficient.java
@@ -2,7 +2,17 @@ package io.acari.dp;
 
 public class BinomialCoefficient {
 
-    public int find(int n, int r){
-        return -1;
+    public long find(int n, int r){
+        final long[][] lookup = new long[n + 1][r + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, r); j++) {
+                if(j == 0 || (i == j) ){
+                    lookup[i][j] = 1;
+                } else {
+                    lookup[i][j] = (lookup[i - 1][j - 1] + lookup[i - 1][j]) % 1000000007L;
+                }
+            }
+        }
+        return lookup[n][r];
     }
 }
