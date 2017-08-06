c6929c83fce009df0fc918662f56d0545031908b
cyclic-reference <alex@cyclicreference.space>
2017-08-06 11:16:02 -0500

    Knotes

--- src/main/java/io/acari/dp/BinomialCoefficient.java
+++ src/main/java/io/acari/dp/BinomialCoefficient.java
@@ -5,7 +5,10 @@ public class BinomialCoefficient {
     public long find(int n, int r) {
         final long[][] lookup = new long[n + 1][r + 1];
         for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= Math.min(i, r); j++) {
+            //This is so that we do not unessessaraly calculate values greater than
+            //N or R when building our lookup table
+            int upperBounds = Math.min(i, r);
+            for (int j = 0; j <= upperBounds; j++) {
                 if (j == 0 || (i == j)) {
                     lookup[i][j] = 1;
                 } else {
