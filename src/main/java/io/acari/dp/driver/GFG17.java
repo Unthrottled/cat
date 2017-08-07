46fa651d3b8e90e8eb120853ef806a7d308af17c
cyclic-reference <alex@cyclicreference.space>
2017-08-06 19:23:15 -0500

    Driver

---
+++ src/main/java/io/acari/dp/driver/GFG17.java
@@ -0,0 +1,18 @@
+package io.acari.dp.driver;
+
+import io.acari.dp.HopCount;
+import io.acari.dp.LongestPalindrome;
+
+import java.util.Scanner;
+
+public class GFG17 {
+    public static void main(String[] args) {
+        HopCount steve = new HopCount();
+        Scanner in = new Scanner(System.in);
+        int t = Integer.parseInt(in.nextLine());
+        for (int i_t = 0; i_t < t; i_t++) {
+            String line = in.nextLine();
+            System.out.println(steve.find(Integer.parseInt(line)));
+        }
+    }
+}
