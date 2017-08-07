e59ae55e9431e43932375dedb3ec0d81e3847aa6
cyclic-reference <alex@cyclicreference.space>
2017-08-07 08:14:53 -0500

    Less majic numbers

--- src/main/java/io/acari/dp/HopCount.java
+++ src/main/java/io/acari/dp/HopCount.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class HopCount {
 
     public long find(int steps){
-        long[][] lookup = new long[steps+1][4];
+        final int numSteps = 3;
+        long[][] lookup = new long[steps+1][numSteps + 1];
         Arrays.fill(lookup[0], 1);
         for (int step = 1; step <= steps ; step++) {
-            for (int hop = 1; hop < 4; hop++) {
+            for (int hop = 1; hop <= numSteps; hop++) {
                 int diffy = step - hop;
-                lookup[step][hop] = ((diffy >= 0) ? lookup[diffy][3] : 0) +
+                lookup[step][hop] = ((diffy >= 0) ? lookup[diffy][numSteps] : 0) +
                         (hop > 0 ? lookup[step][hop - 1] : 0);
             }
         }
-        return lookup[steps][3];
+        return lookup[steps][numSteps];
     }
 }
