fcb730f5c11dc0e284ba45bf5c6c947366f5c176
cyclic-reference <alex@cyclicreference.space>
2017-08-09 18:52:57 -0500

    API

--- src/main/java/io/acari/tree/CycleDetection.java
+++ src/main/java/io/acari/tree/CycleDetection.java
@@ -1,4 +1,40 @@
 package io.acari.tree;
 
+import java.util.*;
+
 public class CycleDetection {
+
+    public boolean hasCycle(int v, LinkedList<Integer>[] alist, boolean[] visi, boolean[] explored) {
+        Set<Integer> visited = new HashSet<>();
+        Deque<Node> queue = new LinkedList<>();
+        int _x = 0;
+        for(List<Integer> gee: alist){
+            visited.clear();
+            queue.clear();
+            queue.offer(new Node(_x, alist[_x++]));
+            while(!queue.isEmpty()){
+                Node currentNode = queue.poll();
+                visited.add(currentNode.data);
+                for(Integer iGuy: currentNode.edges){
+                    if(!visited.contains(iGuy)){
+                        queue.push(new Node(iGuy, alist[iGuy]));
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+class Node {
+    List<Integer> edges;
+    int data;
+
+    Node( int d, List<Integer> e){
+        edges = e;
+        data = d;
+    }
 }
