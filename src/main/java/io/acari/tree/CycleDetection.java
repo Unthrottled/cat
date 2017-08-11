caf0bc3a20af207586292d709bfff35e4514186b
cyclic-reference <alex@cyclicreference.space>
2017-08-11 18:47:49 -0500

    Park

--- src/main/java/io/acari/tree/CycleDetection.java
+++ src/main/java/io/acari/tree/CycleDetection.java
@@ -4,22 +4,75 @@ import java.util.*;
 
 public class CycleDetection {
 
-    public boolean hasCycle(int v, LinkedList<Integer>[] alist, boolean[] visi, boolean[] explored) {
+
+
+    public boolean hasCycle(int v, LinkedList<Integer>[] aList, boolean[] visit, boolean[] exploring) {
+        if(!visit[v]){
+            visit[v] = true;
+
+            for (Integer integer : aList[v]) {
+                if(hasCycle(integer, aList, visit, exploring) || exploring[integer]){
+                    return true;
+                }
+            }
+
+        }
+
+
+        exploring[v] = false;
+        return false;
+    }
+
+    public boolean hasCycleNoRecurse(int v, LinkedList<Integer>[] aList, boolean[] visit, boolean[] explored) {
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> queueHash = new HashSet<>();
+        Deque<Node> queue = new LinkedList<>();
+        int _x = 0;
+        for (List<Integer> gee : aList) {
+            visited.clear();
+            queueHash.clear();
+            queue.clear();
+            queueHash.add(_x);
+            queue.offer(new Node(_x, aList[_x++]));
+            while (!queue.isEmpty()) {
+                Node currentNode = queue.poll();
+                int data = currentNode.data;
+                visited.add(data);
+                for (Integer iGuy : currentNode.edges) {
+                    if (!visited.contains(iGuy)) {
+                        queue.push(new Node(iGuy, aList[iGuy]));
+                        queueHash.add(iGuy);
+                    } else if(queueHash.contains(iGuy)){
+                        return true;
+                    }
+                }
+                queueHash.remove(data);
+            }
+        }
+
+        return false;
+    }
+
+    Boolean isCyclic(int V,LinkedList<Integer>[] alist) {
         Set<Integer> visited = new HashSet<>();
         Deque<Node> queue = new LinkedList<>();
         int _x = 0;
-        for (List<Integer> gee : alist) {
+        for(List<Integer> gee: alist){
             visited.clear();
             queue.clear();
             queue.offer(new Node(_x, alist[_x++]));
-            while (!queue.isEmpty()) {
+            while(!queue.isEmpty()){
                 Node currentNode = queue.poll();
-                visited.add(currentNode.data);
-                for (Integer iGuy : currentNode.edges) {
-                    if (!visited.contains(iGuy)) {
-                        queue.push(new Node(iGuy, alist[iGuy]));
+                int currentNodeData = currentNode.data;
+                visited.add(currentNodeData);
+                for(Integer iGuy: currentNode.edges){
+                    LinkedList<Integer> iGuyEdges = alist[iGuy];
+                    if(!visited.contains(iGuy)){
+                        queue.push(new Node(iGuy, iGuyEdges));
                     } else {
-                        return true;
+                        if(iGuyEdges.stream().anyMatch(a->a== currentNodeData)){
+                            return true;
+                        }
                     }
                 }
             }
