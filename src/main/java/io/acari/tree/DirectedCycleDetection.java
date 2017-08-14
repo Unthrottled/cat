a1ff3a254ee3ebc0ae69fedb1d8cbe593baac4c9
cyclic-reference <alex@cyclicreference.space>
2017-08-14 15:13:44 -0500

    moved

--- src/main/java/io/acari/tree/DirectedCycleDetection.java
+++ src/main/java/io/acari/tree/DirectedCycleDetection.java
@@ -61,38 +61,6 @@ public class DirectedCycleDetection {
 
         return false;
     }
-
-    Boolean isCyclic(int V, LinkedList<Integer>[] aList) {
-        int length = aList.length;
-        boolean[] visited = new boolean[length];
-        for (int i = 0; i < aList.length; i++) {
-            if (!visited[i] && isCyclicHelper(new Node(i, aList[i]), null, visited, aList)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isCyclicHelper(Node visitingNode, Node parent, boolean[] visited, LinkedList<Integer>[] aList) {
-        int currentNodeIndex = visitingNode.data;
-        visited[currentNodeIndex] = true;
-        for (Integer edge : visitingNode.edges) {
-            boolean childIsCyclic = !visited[edge] && isCyclicHelper(new Node(edge, aList[edge]), visitingNode, visited, aList);
-            if (childIsCyclic) {
-                return true;
-            }
-
-            boolean isCyclic = visited[edge] && parent != null && edge != parent.data;
-            if (isCyclic) {
-                return true;
-            }
-
-        }
-
-
-        return false;
-    }
 }
 
 class Node {
--- src/main/java/io/acari/tree/UndirectedCycleDetection.java
+++ src/main/java/io/acari/tree/UndirectedCycleDetection.java
@@ -1,4 +1,37 @@
 package io.acari.tree;
 
+import java.util.LinkedList;
+
 public class UndirectedCycleDetection {
+    Boolean isCyclic(int V, LinkedList<Integer>[] aList) {
+        int length = aList.length;
+        boolean[] visited = new boolean[length];
+        for (int i = 0; i < aList.length; i++) {
+            if (!visited[i] && isCyclicHelper(new Node(i, aList[i]), null, visited, aList)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isCyclicHelper(Node visitingNode, Node parent, boolean[] visited, LinkedList<Integer>[] aList) {
+        int currentNodeIndex = visitingNode.data;
+        visited[currentNodeIndex] = true;
+        for (Integer edge : visitingNode.edges) {
+            boolean childIsCyclic = !visited[edge] && isCyclicHelper(new Node(edge, aList[edge]), visitingNode, visited, aList);
+            if (childIsCyclic) {
+                return true;
+            }
+
+            boolean isCyclic = visited[edge] && parent != null && edge != parent.data;
+            if (isCyclic) {
+                return true;
+            }
+
+        }
+
+
+        return false;
+    }
 }
