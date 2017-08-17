0162667196c1a6cf0bf9a654418fbc16214c81e6
cyclic-reference <alex@cyclicreference.space>
2017-08-17 15:19:27 -0500

    API

--- src/main/java/io/acari/tree/DSR2.java
+++ src/main/java/io/acari/tree/DSR2.java
@@ -1,4 +1,12 @@
 package io.acari.tree;
 
+import io.acari.tree.pojo.Node;
+
+import java.util.stream.Stream;
+
 public class DSR2 {
+
+    public Stream<String> find(Node root, int size){
+        return Stream.empty();
+    }
 }
