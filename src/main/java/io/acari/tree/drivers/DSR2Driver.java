06286ba412082cc210bc83f4de5215234e197273
cyclic-reference <alex@cyclicreference.space>
2017-08-19 16:44:00 -0500

    greased lightning

--- src/main/java/io/acari/tree/drivers/DSR2Driver.java
+++ src/main/java/io/acari/tree/drivers/DSR2Driver.java
@@ -3,6 +3,8 @@ package io.acari.tree.drivers;
 import io.acari.tree.DSR2;
 import io.acari.tree.pojo.Edge;
 import io.acari.tree.pojo.Node;
+import util.InputReader;
+import util.OutputWriter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,11 +13,11 @@ import java.util.stream.Collectors;
 
 public class DSR2Driver {
     public static void main(String... args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine().trim());
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        int testCases = Integer.parseInt(in.readString().trim());
         for (int i = 0; i < testCases; i++) {
-            String[] line = in.nextLine().split(" ");
-            Integer numNodes = Integer.valueOf(line[0]);
+            Integer numNodes = Integer.valueOf(in.readString());
             Map<Integer, Node<Integer>> graph = new HashMap<>(numNodes);
             for (int j = 1; j <= numNodes; j++) {
                 Integer phishes = j;
@@ -23,19 +25,21 @@ public class DSR2Driver {
                 node.value = phishes;
                 graph.put(j, node);
             }
-            Integer numRoads = Integer.valueOf(line[1]);
+            Integer numRoads = Integer.valueOf(in.readString());
             for (int kBra = 0; kBra < numRoads; kBra++) {
-                String[] roadz = in.nextLine().split(" ");
-                Integer weight = Integer.valueOf(roadz[2]);
-                Node<Integer> listNode = graph.get(Integer.valueOf(roadz[0]));
-                Node<Integer> listNode2 = graph.get(Integer.valueOf(roadz[1]));
+                Node<Integer> listNode = graph.get(Integer.valueOf(in.readString()));
+                Node<Integer> listNode2 = graph.get(Integer.valueOf(in.readString()));
+                Integer weight = Integer.valueOf(in.readString());
                 listNode.getEdges().add(new Edge<>(weight, listNode2));
                 listNode2.getEdges().add(new Edge<>(weight, listNode));
             }
 
-            int root = Integer.parseInt(in.nextLine().trim());
-            System.out.println(new DSR2().find(graph.get(root), graph).collect(Collectors.joining(" ")));
+            int root = Integer.parseInt(in.readString().trim());
+            new DSR2().find(graph.get(root), graph)
+                    .forEach(s-> System.out.print(s+" "));
+            out.printLine();
+            out.flush();
         }
-        in.close();
+        out.close();
     }
 }
