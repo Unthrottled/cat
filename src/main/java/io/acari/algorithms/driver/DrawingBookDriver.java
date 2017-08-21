ae711fcdb1cdec730d3869f1f5d9a4b77c4993a6
cyclic-reference <alex@cyclicreference.space>
2017-08-20 20:01:34 -0500

    Driver Impl

--- src/main/java/io/acari/algorithms/driver/DrawingBookDriver.java
+++ src/main/java/io/acari/algorithms/driver/DrawingBookDriver.java
@@ -1,4 +1,13 @@
 package io.acari.algorithms.driver;
 
+import io.acari.algorithms.DrawingBook;
+
+import java.util.Scanner;
+
 public class DrawingBookDriver {
+    public static void main(String... args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(new DrawingBook().find(in.nextLong(), in.nextLong()));
+        in.close();
+    }
 }
