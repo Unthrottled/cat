1a3b37c9bcbc0121330a16915535d09af2546a39
cyclic-reference <alex@cyclicreference.space>
2017-08-21 15:03:22 -0500

    I did it

---
+++ src/main/java/io/acari/tree/ShortestPathToEndMaff.java
@@ -0,0 +1,17 @@
+package io.acari.tree;
+
+public class ShortestPathToEndMaff {
+
+    public long find(Integer root, Integer terminatingNode) {
+        long jumps = -1;
+        while (terminatingNode > 0){
+            if(terminatingNode % 3 == 0){
+                terminatingNode /= 3;
+            } else {
+                terminatingNode--;
+            }
+            jumps++;
+        }
+        return jumps;
+    }
+}
