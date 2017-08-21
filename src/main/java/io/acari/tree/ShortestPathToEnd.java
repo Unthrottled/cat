577fd42bf6d096284283440ad169895c641e6576
cyclic-reference <alex@cyclicreference.space>
2017-08-21 08:30:45 -0500

    API

--- src/main/java/io/acari/tree/ShortestPathToEnd.java
+++ src/main/java/io/acari/tree/ShortestPathToEnd.java
@@ -1,4 +1,10 @@
 package io.acari.tree;
 
+import io.acari.tree.pojo.Node;
+
 public class ShortestPathToEnd {
+
+    public long find(Node<Integer> root, Node<Integer> terminatingNode){
+        return -1L;
+    }
 }
