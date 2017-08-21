bb0fda93ac79ee2441d494dc3f7ad00d651961bd
cyclic-reference <alex@cyclicreference.space>
2017-08-20 19:49:20 -0500

    class impl

--- src/main/java/io/acari/algorithms/CatAndMouse.java
+++ src/main/java/io/acari/algorithms/CatAndMouse.java
@@ -3,6 +3,15 @@ package io.acari.algorithms;
 public class CatAndMouse {
 
     public String find(int catA, int catB, int mouseC){
-        return "";
+        int catADiff = Math.abs(mouseC - catA);
+        int catBDiff = Math.abs(mouseC - catB);
+        switch (Integer.compare(catADiff, catBDiff)){
+            case 1:
+                return "Cat B";
+            case -1:
+                return "Cat A";
+            default:
+                return "Mouse C";
+        }
     }
 }
