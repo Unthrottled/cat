6b9dfd338d7925bdb86ebac09fe4806853a5c47c
cyclic-reference <alex@cyclicreference.space>
2017-08-21 08:43:09 -0500

    driver impl

--- src/main/java/io/acari/tree/drivers/SPEDriver.java
+++ src/main/java/io/acari/tree/drivers/SPEDriver.java
@@ -1,4 +1,45 @@
 package io.acari.tree.drivers;
 
+import io.acari.tree.ShortestPathToEnd;
+import io.acari.tree.pojo.Edge;
+import io.acari.tree.pojo.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class SPEDriver {
+
+    public static void main(String[] butts){
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        ShortestPathToEnd steve = new ShortestPathToEnd();
+        for (int i = 0; i < testCases; i++) {
+            Map<Integer, Node<Integer>> graph = buildGraph(in.nextInt());
+            System.out.println(steve.find(graph.get(1), graph.get(graph.size())));
+        }
+    }
+
+    private static Map<Integer, Node<Integer>> buildGraph(int i) {
+        HashMap<Integer, Node<Integer>> graph = new HashMap<>(i);
+        for (int j = 1; j <= i; j++) {
+            Node<Integer> currentNode = graph.getOrDefault(j, new Node<>(j));
+            int edgeOne = j + 1;
+            if(edgeOne <= i){
+                addEdge(currentNode, graph.getOrDefault(edgeOne, new Node<>(edgeOne)));
+            }
+
+            int edgeTwo = j * 3;
+            if(edgeTwo <= i){
+                addEdge(currentNode, graph.getOrDefault(edgeTwo, new Node<>(edgeTwo)));
+            }
+        }
+
+        return graph;
+    }
+
+    private static void addEdge(Node<Integer> currentNode, Node<Integer> edgeNode) {
+        currentNode.getEdges().add(new Edge<>(1, edgeNode));
+        edgeNode.getEdges().add(new Edge<>(1, currentNode));
+    }
 }
