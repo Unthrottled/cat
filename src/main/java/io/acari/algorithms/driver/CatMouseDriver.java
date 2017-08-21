7b757b35d8212cd3fdd70f302c781fb36d7bb5cd
cyclic-reference <alex@cyclicreference.space>
2017-08-20 19:43:20 -0500

    driver impl

--- src/main/java/io/acari/algorithms/driver/CatMouseDriver.java
+++ src/main/java/io/acari/algorithms/driver/CatMouseDriver.java
@@ -1,4 +1,20 @@
 package io.acari.algorithms.driver;
 
+import io.acari.algorithms.CatAndMouse;
+
+import java.util.Scanner;
+
 public class CatMouseDriver {
+
+    public static void main(String... args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        CatAndMouse steve = new CatAndMouse();
+        for(int i = 0; i < testCases; i++){
+            int catA = in.nextInt();
+            int catB = in.nextInt();
+            int mouseC = in.nextInt();
+            System.out.println(steve.find(catA, catB, mouseC));
+        }
+    }
 }
