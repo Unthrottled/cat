05613b7d73cb487e8ffb79432672513bdabb949c
cyclic-reference <alex@cyclicreference.space>
2017-08-20 19:58:29 -0500

    Driver Class

--- src/main/java/io/acari/algorithms/DrawingBook.java
+++ src/main/java/io/acari/algorithms/DrawingBook.java
@@ -2,7 +2,7 @@ package io.acari.algorithms;
 
 public class DrawingBook {
 
-    public long find(long pageNumber){
+    public long find(long pageCount, long pageNumber){
         return -1;
     }
 }
