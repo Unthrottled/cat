c5775a94b1c624beaad8cc08ad0184de6256bf9d
cyclic-reference <alex@cyclicreference.space>
2017-08-20 20:31:17 -0500

    I deed It

--- src/main/java/io/acari/algorithms/DrawingBook.java
+++ src/main/java/io/acari/algorithms/DrawingBook.java
@@ -2,7 +2,15 @@ package io.acari.algorithms;
 
 public class DrawingBook {
 
-    public long find(long pageCount, long pageNumber){
-        return -1;
+    public long find(long pageCount, long pageNumber) {
+        if (pageNumber == 1 || pageNumber >= pageCount) {
+            return 0;
+        } else if (pageCount - pageNumber <= 2) {
+            return 1;
+        } else {
+            long diffFromOne = pageNumber / 2;
+            long diffFromEnd = (pageCount - pageNumber) / 2;
+            return Math.min(diffFromOne, diffFromEnd);
+        }
     }
 }
