16db2c0491bea3f51572978b494c6ff0037b8fb0
cyclic-reference <alex@cyclicreference.space>
2017-08-24 15:22:31 -0500

    EZPEAZ

--- src/main/java/io/acari/algorithms/CountingValleys.java
+++ src/main/java/io/acari/algorithms/CountingValleys.java
@@ -3,6 +3,18 @@ package io.acari.algorithms;
 public class CountingValleys {
 
     public long find(String steve){
-        return -1L;
+        long valleys = 0;
+        long currentHeight = 0;
+        for (char jerry : steve.toCharArray()) {
+            if(jerry == 'U'){
+                if(currentHeight++ == -1){
+                    valleys++;
+                }
+            } else if(jerry == 'D'){
+                currentHeight--;
+            }
+        }
+
+        return valleys;
     }
 }
