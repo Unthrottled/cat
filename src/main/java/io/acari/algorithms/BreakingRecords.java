69619f0860176c8a0f863007910a42400e953155
cyclic-reference <alex@cyclicreference.space>
2017-08-26 16:18:24 -0500

    API

--- src/main/java/io/acari/algorithms/BreakingRecords.java
+++ src/main/java/io/acari/algorithms/BreakingRecords.java
@@ -1,4 +1,14 @@
 package io.acari.algorithms;
 
+import java.util.stream.IntStream;
+
 public class BreakingRecords {
+
+    public IntStream find(IntStream scores){
+        int low = 0;
+        int high = 0;
+        int max = 0;
+        int min = 0;
+        return IntStream.of(low, high);
+    }
 }
