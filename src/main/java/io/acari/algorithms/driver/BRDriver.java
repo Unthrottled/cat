3998546e5cd71ca34843ca28341a41263e652df7
cyclic-reference <alex@cyclicreference.space>
2017-08-26 16:22:19 -0500

    Driver Impl

--- src/main/java/io/acari/algorithms/driver/BRDriver.java
+++ src/main/java/io/acari/algorithms/driver/BRDriver.java
@@ -1,4 +1,21 @@
 package io.acari.algorithms.driver;
 
+import io.acari.algorithms.BreakingRecords;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class BRDriver {
+
+    public static void main(String... args) {
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+        String[] split = in.nextLine().split(" ");
+        BreakingRecords steve = new BreakingRecords();
+        System.out.println(steve.find(Arrays.stream(split)
+                .mapToInt(Integer::parseInt))
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" ")));
+    }
 }
