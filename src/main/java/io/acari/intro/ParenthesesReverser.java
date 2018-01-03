a43858d324238b014a31022660cd3cceb609773b
DATBOI <alex@cyclicreference.space>
2018-01-03 12:09:40 -0600

    API

--- src/main/java/io/acari/intro/ParenthesesReverser.java
+++ src/main/java/io/acari/intro/ParenthesesReverser.java
@@ -1,4 +1,19 @@
 package io.acari.intro;
 
 public class ParenthesesReverser {
+
+
+  /**
+   * You have a string s that consists of English letters, punctuation marks, whitespace characters, and brackets.
+   * It is guaranteed that the parentheses in s form a regular bracket sequence.
+   * <p>
+   * Your task is to reverse the strings contained in each pair of matching parentheses, starting from the innermost pair.
+   * The results string should not contain any parentheses.
+   *
+   * @param s
+   * @return
+   */
+  String reverseParentheses(String s) {
+    return s;
+  }
 }
