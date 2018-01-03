96f1f7cd54cb4fa789b2507385bd3c014a35fde5
DATBOI <alex@cyclicreference.space>
2018-01-03 11:09:07 -0600

    api

--- src/main/java/io/acari/intro/CommonCharacterCounter.java
+++ src/main/java/io/acari/intro/CommonCharacterCounter.java
@@ -1,4 +1,15 @@
 package io.acari.intro;
 
 public class CommonCharacterCounter {
+
+  /**
+   * Given two strings, find the number of common characters between them
+   * @param s1
+   * @param s2
+   * @return
+   */
+  int commonCharacterCount(String s1, String s2) {
+      return 0;
+  }
+
 }
