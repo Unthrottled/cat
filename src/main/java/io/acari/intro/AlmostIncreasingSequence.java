a6a2f93505a0e32645795947048bbc8a0d8cc4ca
DATBOI <alex@cyclicreference.space>
2018-01-03 09:06:05 -0600

    wip

--- src/main/java/io/acari/intro/AlmostIncreasingSequence.java
+++ src/main/java/io/acari/intro/AlmostIncreasingSequence.java
@@ -4,7 +4,34 @@ public class AlmostIncreasingSequence {
 
 
   boolean almostIncreasingSequence(int[] input){
-    return false;
+    if(input.length < 2){
+      return true;
+    }
+    return almostIncreasingSequenceHelper(input, -1, 0, 0);
+  }
+
+
+  private boolean almostIncreasingSequenceHelper(int[] input, int skipIndex, int previousIndex, int deletions){
+    int currentIndex = previousIndex + 1;
+    currentIndex = currentIndex == skipIndex ? currentIndex + 1 : currentIndex;
+    for(int i = currentIndex; i < input.length; i++){
+      int previous = input[previousIndex];
+      int current = input[currentIndex];
+      if(previous >= current){
+        if(++deletions > 1){
+          return false;
+        }
+        return almostIncreasingSequenceHelper(input, previous, current, deletions) ||
+            almostIncreasingSequenceHelper(input, current, previous, deletions);
+
+      } else {
+        //happy path
+        previousIndex = i;
+        currentIndex = i + 1;
+      }
+    }
+
+    return true;
   }
 
 }
