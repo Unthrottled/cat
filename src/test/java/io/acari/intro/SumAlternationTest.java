3a36392b90b1e0800318f4d00b7ec541845803be
DATBOI <alex@cyclicreference.space>
2018-01-03 15:30:53 -0600

    Hey, that's a test!

---
+++ src/test/java/io/acari/intro/SumAlternationTest.java
@@ -0,0 +1,28 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class SumAlternationTest {
+  private SumAlternation sumAlternation = new SumAlternation();
+
+  @Test
+  public void alternatingSumsOne() throws Exception {
+    assertArrayEquals(new int[]{180, 105}, sumAlternation.alternatingSums(new int[]{50, 60, 60, 45, 70}));
+  }
+
+  @Test
+  public void alternatingSumsTwo() throws Exception {
+    assertArrayEquals(new int[]{100, 50}, sumAlternation.alternatingSums(new int[]{100, 50}));
+  }
+
+  @Test
+  public void alternatingSumsThree() throws Exception {
+    assertArrayEquals(new int[]{80,0}, sumAlternation.alternatingSums(new int[]{80}));
+  }
+
+}
