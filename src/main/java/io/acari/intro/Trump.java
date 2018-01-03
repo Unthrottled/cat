877916fcb36529bba649586c10e3a68aa869129d
DATBOI <alex@cyclicreference.space>
2018-01-03 15:49:45 -0600

    api definition

--- src/main/java/io/acari/intro/Trump.java
+++ src/main/java/io/acari/intro/Trump.java
@@ -1,4 +1,31 @@
 package io.acari.intro;
 
 public class Trump {
+
+  /**
+   * Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+   * <p>
+   * Example
+   * <p>
+   * For:
+   * picture = ["abc","ded"]
+   * <p>
+   * the output should be
+   * <p>
+   * addBorder(picture) =
+   * <p>
+   * ["*****",
+   * <p>
+   * "*abc*",
+   * <p>
+   * "*ded*",
+   * <p>
+   * "*****"]
+   *
+   * @param picture
+   * @return
+   */
+  String[] addBorder(String[] picture) {
+    return picture;
+  }
 }
