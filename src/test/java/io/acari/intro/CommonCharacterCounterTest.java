78ab5f919a11d45035547a54edb87f9047524513
DATBOI <alex@cyclicreference.space>
2018-01-03 11:15:41 -0600

    hey there are some tests

---
+++ src/test/java/io/acari/intro/CommonCharacterCounterTest.java
@@ -0,0 +1,38 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class CommonCharacterCounterTest {
+  CommonCharacterCounter commonCharacterCounter = new CommonCharacterCounter();
+
+  @Test
+  public void commonCharacterCountOne() throws Exception {
+    assertEquals(2, commonCharacterCounter.commonCharacterCount("aabcc", "adcaa"));
+  }
+
+  @Test
+  public void commonCharacterCountTwo() throws Exception {
+    assertEquals(4, commonCharacterCounter.commonCharacterCount("zzzz", "zzzzzz"));
+  }
+
+  @Test
+  public void commonCharacterCountThree() throws Exception {
+    assertEquals(3, commonCharacterCounter.commonCharacterCount("abca", "xfzbac"));
+  }
+
+  @Test
+  public void commonCharacterCountFour() throws Exception {
+    assertEquals(0, commonCharacterCounter.commonCharacterCount("a", "b"));
+  }
+
+  @Test
+  public void commonCharacterCountFive() throws Exception {
+    assertEquals(1, commonCharacterCounter.commonCharacterCount("a", "aaa"));
+  }
+
+}
