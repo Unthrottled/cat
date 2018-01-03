b8616c10ee91e9d2e5ce482e12bed56f5f0b29c7
DATBOI <alex@cyclicreference.space>
2018-01-03 11:53:52 -0600

    api

--- src/main/java/io/acari/intro/HeightSorter.java
+++ src/main/java/io/acari/intro/HeightSorter.java
@@ -1,4 +1,17 @@
 package io.acari.intro;
 
 public class HeightSorter {
+
+  /**
+   * Some people are standing in a row in a park.
+   * There are trees between them which cannot be moved.
+   * Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+   *
+   * @param a
+   * @return
+   */
+  int[] sortByHeight(int[] a) {
+    return null;
+  }
+
 }
