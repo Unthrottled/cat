a9e9074c8936f33df1fb09b66d8ed4e98e45ba1b
DATBOI <alex@cyclicreference.space>
2018-01-03 11:38:22 -0600

    hey there ar the tests

---
+++ src/test/java/io/acari/intro/LuckNumberTest.java
@@ -0,0 +1,43 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class LuckNumberTest {
+  private LuckNumber luckNumber = new LuckNumber();
+
+  @Test
+  public void isLuckyOne() throws Exception {
+    assertTrue(luckNumber.isLucky(1230));
+  }
+
+  @Test
+  public void isLuckyTwo() throws Exception {
+    assertFalse(luckNumber.isLucky(239017));
+  }
+
+  @Test
+  public void isLuckyThree() throws Exception {
+    assertTrue(luckNumber.isLucky(134008));
+  }
+
+  @Test
+  public void isLuckyFour() throws Exception {
+    assertFalse(luckNumber.isLucky(10));
+  }
+
+  @Test
+  public void isLuckyFive() throws Exception {
+    assertTrue(luckNumber.isLucky(11));
+  }
+
+  @Test
+  public void isLuckySix() throws Exception {
+    assertTrue(luckNumber.isLucky(1010));
+  }
+
+}
