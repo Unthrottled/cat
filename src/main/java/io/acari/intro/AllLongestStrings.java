81d627f490a90665bc0cd4192af600822d9590e9
DATBOI <alex@cyclicreference.space>
2018-01-03 10:42:52 -0600

    api

--- src/main/java/io/acari/intro/AllLongestStrings.java
+++ src/main/java/io/acari/intro/AllLongestStrings.java
@@ -1,4 +1,17 @@
 package io.acari.intro;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class AllLongestStrings {
+
+  /**
+   * Given an array of strings, return another array containing all of its longest strings.
+   *
+   * @param inputArray
+   * @return
+   */
+  String[] allLongestStrings(String[] inputArray) {
+    return null;
+  }
 }
