5715dcd0d1cc23858645a56903cc333a3c06dc4b
DATBOI <alex@cyclicreference.space>
2018-01-04 11:55:06 -0600

    api

--- src/main/java/io/acari/intro/Judge.java
+++ src/main/java/io/acari/intro/Judge.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class Judge {
+
+  /**
+   * Two arrays are called similar if one can be obtained from another
+   * by swapping at most one pair of elements in one of the arrays.
+   * <p>
+   * Given two arrays a and b, check whether they are similar.
+   *
+   * @param a
+   * @param b
+   * @return
+   */
+  boolean areSimilar(int[] a, int[] b) {
+    return false;
+  }
 }
