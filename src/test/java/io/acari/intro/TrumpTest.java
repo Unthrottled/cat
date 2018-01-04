e1df153465d3c8f218cfb061ce99a5c9b3d3b4f9
DATBOI <alex@cyclicreference.space>
2018-01-04 11:43:48 -0600

    Hey thats a test

---
+++ src/test/java/io/acari/intro/TrumpTest.java
@@ -0,0 +1,31 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class TrumpTest {
+  private Trump trump = new Trump();
+
+  @Test
+  public void addBorderOne() throws Exception {
+    assertArrayEquals(new String[]{"*****",
+        "*abc*",
+        "*ded*",
+        "*****"},
+        trump.addBorder(new String[]{"abc",
+        "ded"}));
+  }
+
+  @Test
+  public void addBorderTwo() throws Exception {
+    assertArrayEquals(new String[]{"***",
+        "*a*",
+        "***"},
+        trump.addBorder(new String[]{"a"}));
+  }
+
+}
