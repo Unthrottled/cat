031ffa11e4da1fcb41523c7b5a9a4179f3c441d1
DATBOI <alex@cyclicreference.space>
2018-01-04 15:09:12 -0600

    api definition

--- src/main/java/io/acari/intro/TacoCat.java
+++ src/main/java/io/acari/intro/TacoCat.java
@@ -2,4 +2,15 @@ package io.acari.intro;
 
 public class TacoCat {
 
+  /**
+   * Given a string, find out if its characters
+   * can be rearranged to form a palindrome.
+   *
+   * @param inputString
+   * @return
+   */
+  boolean palindromeRearranging(String inputString) {
+    return false;
+  }
+
 }
