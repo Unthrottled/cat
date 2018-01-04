80faa4af57640d461a4cf1ed42d3804447b37c61
DATBOI <alex@cyclicreference.space>
2018-01-04 13:32:12 -0600

    api definition

--- src/main/java/io/acari/intro/ArrayChange.java
+++ src/main/java/io/acari/intro/ArrayChange.java
@@ -1,4 +1,19 @@
 package io.acari.intro;
 
 public class ArrayChange {
+
+  /**
+   * You are given an array of integers.
+   * On each move you are allowed to increase exactly
+   * one of its element by one.
+   * <p>
+   * Find the minimal number of moves required to
+   * obtain a strictly increasing sequence from the input.
+   *
+   * @param inputArray
+   * @return
+   */
+  int arrayChange(int[] inputArray) {
+    return 0;
+  }
 }
