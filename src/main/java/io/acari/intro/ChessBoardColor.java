2af81f881d9e5c40eff6a7c592d1f3fb235d8931
DATBOI <alex@cyclicreference.space>
2018-01-05 15:10:28 -0600

    Clazz

---
+++ src/main/java/io/acari/intro/ChessBoardColor.java
@@ -0,0 +1,14 @@
+package io.acari.intro;
+
+public class ChessBoardColor {
+
+  /**
+   * Given two cells on the standard chess board, determine whether they have the same color or not.
+   * @param cell1
+   * @param cell2
+   * @return
+   */
+  boolean chessBoardCellColor(String cell1, String cell2) {
+    return false;
+  }
+}
