ca2794a6c097e3c6ed039cf4bd9438ef40574c81
DATBOI <alex@cyclicreference.space>
2018-01-05 11:11:18 -0600

    that api definition

--- src/main/java/io/acari/intro/IPv4Validator.java
+++ src/main/java/io/acari/intro/IPv4Validator.java
@@ -1,4 +1,25 @@
 package io.acari.intro;
 
 public class IPv4Validator {
+
+
+  /**
+   * An IP address is a numerical label assigned to each device
+   * (e.g., computer, printer) participating in a computer network
+   * that uses the Internet Protocol for communication.
+   *
+   * There are two versions of the Internet protocol, and thus two versions of addresses.
+   * One of them is the IPv4 address.
+   *
+   * IPv4 addresses are represented in dot-decimal notation,
+   * which consists of four decimal numbers, each ranging
+   * from 0 to 255 inclusive, separated by dots, e.g., 172.16.254.1.
+   *
+   * Given a string, find out if it satisfies the IPv4 address naming rules.
+   * @param inputString
+   * @return
+   */
+  boolean isIPv4Address(String inputString) {
+    return false;
+  }
 }
