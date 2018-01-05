22f81ad033dcdcb58a44b8bc6909cb7805e7c4c2
DATBOI <alex@cyclicreference.space>
2018-01-05 14:44:36 -0600

    API

---
+++ src/main/java/io/acari/intro/AlphaShift.java
@@ -0,0 +1,18 @@
+package io.acari.intro;
+
+public class AlphaShift {
+
+  /**
+   * Given a string, replace each its
+   * character by the next one in the English alphabet (z would be replaced by a).
+   *
+   * @param inputString
+   * @return
+   */
+  String alphabeticShift(String inputString) {
+    StringBuilder stringBuilder = new StringBuilder();
+    return stringBuilder.toString();
+  }
+
+
+}
