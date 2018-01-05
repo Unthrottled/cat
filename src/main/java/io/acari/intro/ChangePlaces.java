863d712b3baa1a5504f41708cffb2014bb76c002
DATBOI <alex@cyclicreference.space>
2018-01-05 14:13:10 -0600

    is that it?

--- src/main/java/io/acari/intro/ChangePlaces.java
+++ src/main/java/io/acari/intro/ChangePlaces.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class ChangePlaces {
+
+  /**
+   * Given an array of integers,
+   * replace all the occurrences of elemToReplace with substitutionElem.
+   *
+   * @param inputArray
+   * @param elemToReplace
+   * @param substitutionElem
+   * @return
+   */
+  int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+    return inputArray;
+  }
+
 }
