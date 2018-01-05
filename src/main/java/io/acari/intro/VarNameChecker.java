20d7b33355fc24aed4738637d0dde7d106e94359
DATBOI <alex@cyclicreference.space>
2018-01-05 14:21:40 -0600

    api

--- src/main/java/io/acari/intro/VarNameChecker.java
+++ src/main/java/io/acari/intro/VarNameChecker.java
@@ -1,4 +1,16 @@
 package io.acari.intro;
 
 public class VarNameChecker {
+
+  /**
+   * Correct variable names consist only of Latin letters,
+   * digits and underscores and they can't start with a digit.
+   *
+   * @param name
+   * @return
+   */
+  boolean variableName(String name) {
+    return false;
+  }
+
 }
