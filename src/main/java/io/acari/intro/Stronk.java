2e7fe23ee9a68461158bb07931ec834074fbc700
DATBOI <alex@cyclicreference.space>
2018-01-05 10:09:30 -0600

    api definition!

--- src/main/java/io/acari/intro/Stronk.java
+++ src/main/java/io/acari/intro/Stronk.java
@@ -1,4 +1,22 @@
 package io.acari.intro;
 
 public class Stronk {
+
+  /**
+   * Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+   * <p>
+   * Call two people equally strong if their strongest arms are equally strong
+   * (the strongest arm can be both the right and the left), and so are their weakest arms.
+   * <p>
+   * Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+   *
+   * @param yourLeft
+   * @param yourRight
+   * @param friendsLeft
+   * @param friendsRight
+   * @return
+   */
+  boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
+    return false;
+  }
 }
