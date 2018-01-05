de17643c16df908ff23acd243b9f58189f3c7a99
DATBOI <alex@cyclicreference.space>
2018-01-05 15:51:41 -0600

    darn that floating point loss

---
+++ src/main/java/io/acari/intro/Deposit.java
@@ -0,0 +1,15 @@
+package io.acari.intro;
+
+public class Deposit {
+
+  int depositProfit(int deposit, int rate, int threshold){
+    int years = 0;
+    double newDeposit = deposit;
+    while(Double.compare(newDeposit, (double) threshold) < 0){
+      years++;
+      newDeposit  += newDeposit * (rate / 100d);
+    }
+    return years;
+  }
+
+}
---
+++ src/test/java/io/acari/intro/DepositTest.java
@@ -0,0 +1,16 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class DepositTest {
+  @Test
+  public void depositProfit() throws Exception {
+    assertEquals(3, new Deposit().depositProfit(100, 20, 170));
+  }
+
+}
