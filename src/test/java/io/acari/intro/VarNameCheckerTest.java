7d940188823c7e708d37d9de75fb2e8dd2a5e39f
DATBOI <alex@cyclicreference.space>
2018-01-05 14:30:21 -0600

    tests!

---
+++ src/test/java/io/acari/intro/VarNameCheckerTest.java
@@ -0,0 +1,38 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class VarNameCheckerTest {
+  private VarNameChecker varNameChecker = new VarNameChecker();
+
+  @Test
+  public void variableNameOne() throws Exception {
+    assertTrue(varNameChecker.variableName("var_1__Int"));
+  }
+
+  @Test
+  public void variableNameTwo() throws Exception {
+    assertFalse(varNameChecker.variableName("qq-q"));
+  }
+
+  @Test
+  public void variableNameThree() throws Exception {
+    assertFalse(varNameChecker.variableName("2w2"));
+  }
+
+  @Test
+  public void variableNameFour() throws Exception {
+    assertFalse(varNameChecker.variableName(" variable"));
+  }
+
+  @Test
+  public void variableNameFive() throws Exception {
+    assertFalse(varNameChecker.variableName("va[riable0"));
+  }
+
+}
