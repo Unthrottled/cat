3ce482a3ec01ae97511d5fe0ebf543db1ac9e8b7
DATBOI <alex@cyclicreference.space>
2018-01-05 10:51:27 -0600

    that api bouy

--- src/main/java/io/acari/intro/AdjacentLists.java
+++ src/main/java/io/acari/intro/AdjacentLists.java
@@ -1,4 +1,15 @@
 package io.acari.intro;
 
 public class AdjacentLists {
+
+  /**
+   * Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+   * @param inputArray
+   * @return the maximal absolute difference between any two of its adjacent elements.
+   */
+  int arrayMaximalAdjacentDifference(int[] inputArray) {
+    return 0;
+  }
+
+
 }
