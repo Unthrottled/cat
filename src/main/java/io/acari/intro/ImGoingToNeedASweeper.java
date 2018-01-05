31d94df5861b7bfbb96674d910232730c3306a46
DATBOI <alex@cyclicreference.space>
2018-01-05 13:13:52 -0600

    It helps to have an api definition!

--- src/main/java/io/acari/intro/ImGoingToNeedASweeper.java
+++ src/main/java/io/acari/intro/ImGoingToNeedASweeper.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class ImGoingToNeedASweeper {
+
+  /**
+   * In the popular Minesweeper game you have a board with some mines and
+   * those cells that don't contain a mine have a number in it that indicates the
+   * total number of mines in the neighboring cells.
+   * <p>
+   * Starting off with some arrangement of mines we want to create a Minesweeper game setup.
+   *
+   * @param matrix
+   * @return
+   */
+  int[][] minesweeper(boolean[][] matrix) {
+    return null;
+  }
 }
