0b7cbd2cb8ce275f1e3d1bb7ba03c336b746d443
DATBOI <alex@cyclicreference.space>
2018-01-05 14:15:24 -0600

    not going to bother with test

--- src/main/java/io/acari/intro/ChangePlaces.java
+++ src/main/java/io/acari/intro/ChangePlaces.java
@@ -12,6 +12,11 @@ public class ChangePlaces {
    * @return
    */
   int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+    for (int i = 0; i < inputArray.length; i++) {
+      if(inputArray[i] == elemToReplace){
+        inputArray[i] = substitutionElem;
+      }
+    }
     return inputArray;
   }
 
