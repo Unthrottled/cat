8424f05a7f537f575898ee0c698993505c7e0935
DATBOI <alex@cyclicreference.space>
2018-01-05 14:18:25 -0600

    come on

---
+++ src/main/java/io/acari/intro/AllEven.java
@@ -0,0 +1,18 @@
+package io.acari.intro;
+
+public class AllEven {
+
+  boolean evenDigitsOnly(int n){
+    do {
+      int digit = n % 10;
+      if(isEven(digit))
+        return false;
+      n /=10;
+    } while (n > 0);
+    return true;
+  }
+
+  private boolean isEven(int digit) {
+    return digit%2 != 0;
+  }
+}
