c4acb71fd7288be6e232a656a4f264fb422ba73f
DATBOI <alex@cyclicreference.space>
2018-01-06 17:46:48 -0600

    API

--- src/main/java/io/acari/intro/AbsoluteMin.java
+++ src/main/java/io/acari/intro/AbsoluteMin.java
@@ -1,4 +1,23 @@
 package io.acari.intro;
 
 public class AbsoluteMin {
+
+
+  /**
+   * Given a sorted array of integers a, find an integer x
+   * from a such that the value of
+   * <p>
+   * abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+   * <p>
+   * is the smallest possible (here abs denotes the absolute value).
+   * <p>
+   * If there are several possible answers, output the smallest one.
+   *
+   * @param a
+   * @return
+   */
+  int absoluteValuesSumMinimization(int[] a) {
+    return 0;
+  }
+
 }
