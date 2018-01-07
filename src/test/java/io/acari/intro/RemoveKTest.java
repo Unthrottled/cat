8dfbe15cc7b47afb8d7c413077e06da1974be774
DATBOI <alex@cyclicreference.space>
2018-01-07 17:12:20 -0600

    testsgit add .!

---
+++ src/test/java/io/acari/intro/RemoveKTest.java
@@ -0,0 +1,38 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class RemoveKTest {
+  private RemoveK removeK = new RemoveK();
+
+
+  @Test
+  public void extractEachKthOne() throws Exception {
+    assertArrayEquals(
+        new int[]{1, 2, 4, 5, 7, 8, 10},
+        removeK.extractEachKth(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3)
+        );
+  }
+
+  @Test
+  public void extractEachKthTwo() throws Exception {
+    assertArrayEquals(
+        new int[]{},
+        removeK.extractEachKth(new int[]{1, 1, 1, 1, 1}, 1)
+        );
+  }
+
+  @Test
+  public void extractEachKthThree() throws Exception {
+    assertArrayEquals(
+        new int[]{1,1,1,1},
+        removeK.extractEachKth(new int[]{1, 2, 1, 2, 1, 2, 1, 2}, 2)
+        );
+  }
+
+}
