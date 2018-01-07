552ebb3b88c70b6aa1031270738a419f6b6b5b39
DATBOI <alex@cyclicreference.space>
2018-01-07 17:41:35 -0600

    api

--- src/main/java/io/acari/intro/DigitFirst.java
+++ src/main/java/io/acari/intro/DigitFirst.java
@@ -1,4 +1,14 @@
 package io.acari.intro;
 
 public class DigitFirst {
+
+  /**
+   * Find the leftmost digit that occurs in a given string.
+   *
+   * @param inputString
+   * @return
+   */
+  char firstDigit(String inputString) {
+    return '\0';
+  }
 }
