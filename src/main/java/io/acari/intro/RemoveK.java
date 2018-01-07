e7e2760a0ed51a7e839d600058ef6dd28d5dc821
DATBOI <alex@cyclicreference.space>
2018-01-07 17:08:21 -0600

    api

--- src/main/java/io/acari/intro/RemoveK.java
+++ src/main/java/io/acari/intro/RemoveK.java
@@ -2,5 +2,15 @@ package io.acari.intro;
 
 public class RemoveK {
 
+  /**
+   * Given array of integers, remove each kth element from it.
+   *
+   * @param inputArray
+   * @param k
+   * @return
+   */
+  int[] extractEachKth(int[] inputArray, int k) {
+    return inputArray;
+  }
 
 }
