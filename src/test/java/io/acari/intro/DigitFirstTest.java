19428778e90204732ead2b489bc0cdb20d7c0f19
DATBOI <alex@cyclicreference.space>
2018-01-07 17:42:36 -0600

    that test boi

---
+++ src/test/java/io/acari/intro/DigitFirstTest.java
@@ -0,0 +1,16 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class DigitFirstTest {
+  @Test
+  public void firstDigit() throws Exception {
+    assertEquals('1', new DigitFirst().firstDigit("var_1__Int"));
+  }
+
+}
