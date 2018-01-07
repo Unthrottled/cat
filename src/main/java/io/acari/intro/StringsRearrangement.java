4756de0a42d5f50b8676c4bfa558fe5a48e089ab
DATBOI <alex@cyclicreference.space>
2018-01-07 16:19:15 -0600

    API

--- src/main/java/io/acari/intro/StringsRearrangement.java
+++ src/main/java/io/acari/intro/StringsRearrangement.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class StringsRearrangement {
+
+  /**
+   * Given an array of equal-length strings,
+   * check if it is possible to rearrange the strings
+   * in such a way that after the rearrangement the strings
+   * at consecutive positions would differ by exactly one character.
+   *
+   * @param inputArray
+   * @return
+   */
+  boolean stringsRearrangement(String[] inputArray) {
+    return false;
+  }
+
 }
