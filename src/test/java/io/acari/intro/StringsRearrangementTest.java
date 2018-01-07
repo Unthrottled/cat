da05b10f5d70bea67d2ebcc7c39279747790275b
DATBOI <alex@cyclicreference.space>
2018-01-07 16:25:03 -0600

    Some tests for this problem

---
+++ src/test/java/io/acari/intro/StringsRearrangementTest.java
@@ -0,0 +1,73 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class StringsRearrangementTest {
+  private StringsRearrangement stringsRearrangement = new StringsRearrangement();
+  @Test
+  public void stringsRearrangementOne() throws Exception {
+    assertFalse(stringsRearrangement.stringsRearrangement(new String[]{"aba",
+        "bbb",
+        "bab"}));
+  }
+
+  @Test
+  public void stringsRearrangementTwo() throws Exception {
+    assertTrue(stringsRearrangement.stringsRearrangement(new String[]{"ab",
+        "bb",
+        "aa"}));
+  }
+
+  @Test
+  public void stringsRearrangementThree() throws Exception {
+    assertFalse(stringsRearrangement.stringsRearrangement(new String[]{"q", "q"}));
+  }
+
+  @Test
+  public void stringsRearrangementFour() throws Exception {
+    assertTrue(stringsRearrangement.stringsRearrangement(new String[]{"zzzzab",
+        "zzzzbb",
+        "zzzzaa"}));
+  }
+
+  @Test
+  public void stringsRearrangementFive() throws Exception {
+    assertTrue(stringsRearrangement.stringsRearrangement(new String[]{"ab",
+        "ad",
+        "ef",
+        "eg"}));
+  }
+
+  @Test
+  public void stringsRearrangementSix() throws Exception {
+    assertTrue(stringsRearrangement.stringsRearrangement(new String[]{"abc",
+        "bef",
+        "bcc",
+        "bec",
+        "bbc",
+        "bdc"}));
+  }
+
+  @Test
+  public void stringsRearrangementSeven() throws Exception {
+    assertFalse(stringsRearrangement.stringsRearrangement(new String[]{"abc",
+        "abx",
+        "axx",
+        "abc"}));
+  }
+
+  @Test
+  public void stringsRearrangementEight() throws Exception {
+    assertTrue(stringsRearrangement.stringsRearrangement(new String[]{"abc",
+        "abx",
+        "axx",
+        "abx",
+        "abc"}));
+  }
+
+}
