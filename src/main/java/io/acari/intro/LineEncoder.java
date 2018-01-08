daded7d39833313549ac5119ba915af860045458
DATBOI <alex@cyclicreference.space>
2018-01-08 15:44:05 -0600

    Words above a method signature

--- src/main/java/io/acari/intro/LineEncoder.java
+++ src/main/java/io/acari/intro/LineEncoder.java
@@ -2,5 +2,26 @@ package io.acari.intro;
 
 public class LineEncoder {
 
+  /**
+   * Given a string, return its encoding defined as follows:
+   * <p>
+   * First, the string is divided into the least possible
+   * number of disjoint substrings consisting of identical character
+   * for example, "aabbbc" is divided into ["aa", "bbb", "c"]
+   * <p>
+   * Next, each substring with length greater than one is
+   * replaced with a concatenation of its length and the
+   * repeating character for example, substring "bbb" is
+   * replaced by "3b"
+   * <p>
+   * Finally, all the new strings are concatenated together
+   * in the same order and a new string is returned.
+   *
+   * @param s
+   * @return
+   */
+  String lineEncoding(String s) {
+    return s;
+  }
 
 }
