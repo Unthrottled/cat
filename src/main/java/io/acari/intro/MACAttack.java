8ea47c2366f2b94800a99289c87a7bc83f612af4
DATBOI <alex@cyclicreference.space>
2018-01-08 15:28:37 -0600

    hey that looks like a problem statement

--- src/main/java/io/acari/intro/MACAttack.java
+++ src/main/java/io/acari/intro/MACAttack.java
@@ -1,4 +1,21 @@
 package io.acari.intro;
 
 public class MACAttack {
+
+  /**
+   * A media access control address (MAC address)
+   * is a unique identifier assigned to network interfaces
+   * for communications on the physical network segment.
+   * <p>
+   * The standard (IEEE 802) format for printing MAC-48
+   * addresses in human-friendly form is six groups of
+   * two hexadecimal digits (0 to 9 or A to F), separated
+   * by hyphens (e.g. 01-23-45-67-89-AB).
+   *
+   * @param inputString
+   * @return
+   */
+  boolean isMAC48Address(String inputString) {
+    return false;
+  }
 }
