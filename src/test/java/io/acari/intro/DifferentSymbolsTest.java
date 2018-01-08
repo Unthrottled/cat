d13e8a7457dabe8cf3f2411077f053c629bdd784
DATBOI <alex@cyclicreference.space>
2018-01-07 18:16:30 -0600

    ez test

---
+++ src/test/java/io/acari/intro/DifferentSymbolsTest.java
@@ -0,0 +1,16 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class DifferentSymbolsTest {
+  @Test
+  public void differentSymbolsNaive() throws Exception {
+    assertEquals(3, new DifferentSymbols().differentSymbolsNaive("cabca"));
+  }
+
+}
