b80561c7430be03073a2dc330c047fd84fa08fc8
DATBOI <alexsimons9999@gmail.com>
2018-01-07 18:44:30 -0600

    api is defined

--- src/main/java/io/acari/intro/PlantsGrowYo.java
+++ src/main/java/io/acari/intro/PlantsGrowYo.java
@@ -1,4 +1,26 @@
 package io.acari.intro;
 
 public class PlantsGrowYo {
+
+    /**
+     * Each day a plant is growing by upSpeed meters.
+     * <p>
+     * Each night that plant's height decreases by downSpeed
+     * meters due to the lack of sun heat.
+     * <p>
+     * Initially, plant is 0 meters tall.
+     * <p>
+     * We plant the seed at the beginning of a day.
+     * <p>
+     * We want to know when the height of the plant will
+     * reach a certain level.
+     *
+     * @param upSpeed
+     * @param downSpeed
+     * @param desiredHeight
+     * @return
+     */
+    int growingPlant(int upSpeed, int downSpeed, int desiredHeight) {
+        return 0;
+    }
 }
