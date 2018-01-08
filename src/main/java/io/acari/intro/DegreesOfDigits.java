b3c575739f098d1f5f47d5bbbf5d03615a422e3c
DATBOI <alex@cyclicreference.space>
2018-01-08 09:03:39 -0600

    API definition

--- src/main/java/io/acari/intro/DegreesOfDigits.java
+++ src/main/java/io/acari/intro/DegreesOfDigits.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class DegreesOfDigits {
+
+  /**
+   * Let's define digit degree of some positive integer
+   * as the number of times we need to replace this number
+   * with the sum of its digits until we get to a one digit number.
+   * <p>
+   * Given an integer, find its digit degree.
+   *
+   * @param n
+   * @return
+   */
+  int digitDegree(int n) {
+    return n;
+  }
 }
