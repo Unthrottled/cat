786f88137166d2c95342661c89320f81a03992fe
DATBOI <alex@cyclicreference.space>
2018-01-08 08:51:55 -0600

    documented API

--- src/main/java/io/acari/intro/LongestPrefix.java
+++ src/main/java/io/acari/intro/LongestPrefix.java
@@ -1,4 +1,15 @@
 package io.acari.intro;
 
 public class LongestPrefix {
+
+  /**
+   * Given a string, output its longest prefix which
+   * contains only digits.
+   *
+   * @param inputString
+   * @return
+   */
+  String longestDigitsPrefix(String inputString) {
+    return inputString;
+  }
 }
