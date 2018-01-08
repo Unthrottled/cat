8237ca5eea3b5100b5a868ae070de7b2a26ae7d3
DATBOI <alex@cyclicreference.space>
2018-01-08 10:21:26 -0600

    some Tests

---
+++ src/test/java/io/acari/intro/BishopNPrawnTest.java
@@ -0,0 +1,43 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class BishopNPrawnTest {
+  private final BishopNPrawn bishopNPrawn = new BishopNPrawn();
+
+  @Test
+  public void bishopAndPawnOne() throws Exception {
+    assertTrue(bishopNPrawn.bishopAndPawn("a1", "c3"));
+  }
+
+  @Test
+  public void bishopAndPawnTwo() throws Exception {
+    assertFalse(bishopNPrawn.bishopAndPawn("h1", "h3"));
+  }
+
+  @Test
+  public void bishopAndPawnThree() throws Exception {
+    assertTrue(bishopNPrawn.bishopAndPawn("a5", "c3"));
+  }
+
+  @Test
+  public void bishopAndPawnFour() throws Exception {
+    assertFalse(bishopNPrawn.bishopAndPawn("g1", "f3"));
+  }
+
+  @Test
+  public void bishopAndPawnFive() throws Exception {
+    assertFalse(bishopNPrawn.bishopAndPawn("e7", "d6"));
+  }
+
+  @Test
+  public void bishopAndPawnSix() throws Exception {
+    assertTrue(bishopNPrawn.bishopAndPawn("e7", "a3"));
+  }
+
+}
