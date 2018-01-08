497bade0396a60b67c95b40883034c8e5658fcba
DATBOI <alex@cyclicreference.space>
2018-01-07 18:15:41 -0600

    API

--- src/main/java/io/acari/intro/DifferentSymbols.java
+++ src/main/java/io/acari/intro/DifferentSymbols.java
@@ -1,4 +1,13 @@
 package io.acari.intro;
 
 public class DifferentSymbols {
+
+  /**
+   * Given a string, find the number of different characters in it.
+   * @param s
+   * @return
+   */
+  int differentSymbolsNaive(String s) {
+    return s.length();
+  }
 }
