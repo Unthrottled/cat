b546a62240466e3334c39100fb1783f3cbfcfda0
DATBOI <alex@cyclicreference.space>
2018-01-08 11:47:11 -0600

    why you so eazy to impl?

--- src/main/java/io/acari/intro/StringBoutique.java
+++ src/main/java/io/acari/intro/StringBoutique.java
@@ -10,6 +10,13 @@ public class StringBoutique {
    * @return
    */
   boolean isBeautifulString(String inputString) {
+    int[] charCounts = new int[26];
+    inputString.chars().map(i->i-97).forEach(i->charCounts[i]++);
+    for (int i = 1; i < charCounts.length; i++) {
+      if(charCounts[i-1] < charCounts[i]){
+        return false;
+      }
+    }
     return true;//gurl you always beautiful.
   }
 
