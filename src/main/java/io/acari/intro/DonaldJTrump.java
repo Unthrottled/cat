b6d260a8d2b11e96ee6c87e70fb67a60de1ce37d
DATBOI <alex@cyclicreference.space>
2018-01-08 14:55:51 -0600

    API definiton bruv

--- src/main/java/io/acari/intro/DonaldJTrump.java
+++ src/main/java/io/acari/intro/DonaldJTrump.java
@@ -1,4 +1,26 @@
 package io.acari.intro;
 
 public class DonaldJTrump {
+
+  /**
+   * Elections are in progress!
+   * Given an array of the numbers of votes given
+   * to each of the candidates so far, and an integer
+   * k equal to the number of voters who haven't cast
+   * their vote yet, find the number of candidates
+   * who still have a chance to win the election.
+   * <p>
+   * The winner of the election must secure
+   * strictly more votes than any other candidate.
+   * <p>
+   * If two or more candidates receive the same
+   * (maximum) number of votes, assume there is no winner at all.
+   *
+   * @param votes
+   * @param k
+   * @return
+   */
+  int electionsWinners(int[] votes, int k) {
+    return 0;
+  }
 }
