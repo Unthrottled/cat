de6d7f99a3bb761d3a0ad6aceef0021bc4c8d3e4
DATBOI <alex@cyclicreference.space>
2018-01-08 08:21:38 -0600

    documented api

--- src/main/java/io/acari/intro/KnapsackLite.java
+++ src/main/java/io/acari/intro/KnapsackLite.java
@@ -1,4 +1,23 @@
 package io.acari.intro;
 
 public class KnapsackLite {
+
+  /**
+   * You found two items in a treasure chest!
+   * The first item weighs weight1 and is worth value1, and the second
+   * item weighs weight2 and is worth value2. What is the total maximum value of the
+   * items you can take with you,
+   * assuming that your max weight capacity is maxW and
+   * you can't come back for the items later?
+   *
+   * @param value1
+   * @param weight1
+   * @param value2
+   * @param weight2
+   * @param maxW
+   * @return
+   */
+  int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+    return 0;
+  }
 }
