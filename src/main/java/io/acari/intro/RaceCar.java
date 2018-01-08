4c3f1847f1cd62cd67ec2cfca7d006c096728209
DATBOI <alex@cyclicreference.space>
2018-01-08 11:59:18 -0600

    look at that api definition!

--- src/main/java/io/acari/intro/RaceCar.java
+++ src/main/java/io/acari/intro/RaceCar.java
@@ -1,4 +1,17 @@
 package io.acari.intro;
 
 public class RaceCar {
+
+  /**
+   * Given a string, find the shortest possible
+   * string which can be achieved by adding characters
+   * to the end of initial string to make it a palindrome.
+   *
+   * @param st
+   * @return
+   */
+  String buildPalindrome(String st) {
+    return st;
+  }
+
 }
