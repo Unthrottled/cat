35f066a716bac4d1ac34178a3b0e148c8730f8b0
DATBOI <alex@cyclicreference.space>
2018-01-08 11:43:49 -0600

    don't get testy with me

---
+++ src/test/java/io/acari/intro/StringBoutiqueTest.java
@@ -0,0 +1,38 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class StringBoutiqueTest {
+  private final StringBoutique stringBoutique = new StringBoutique();
+
+  @Test
+  public void isBeautifulStringOne() throws Exception {
+    assertTrue(stringBoutique.isBeautifulString("bbbaacdafe"));
+  }
+
+  @Test
+  public void isBeautifulStringTwo() throws Exception {
+    assertFalse(stringBoutique.isBeautifulString("aabbb"));
+  }
+
+  @Test
+  public void isBeautifulStringThree() throws Exception {
+    assertFalse(stringBoutique.isBeautifulString("bbc"));
+  }
+
+  @Test
+  public void isBeautifulStringFour() throws Exception {
+    assertFalse(stringBoutique.isBeautifulString("bbbaa"));
+  }
+
+  @Test
+  public void isBeautifulStringFive() throws Exception {
+    assertFalse(stringBoutique.isBeautifulString("abcdefghijklmnopqrstuvwxyzz"));
+  }
+
+}
