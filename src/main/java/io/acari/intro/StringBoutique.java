923e9579b686cf150721842109be7c3a028c5e91
DATBOI <alex@cyclicreference.space>
2018-01-08 11:41:05 -0600

    API definition

--- src/main/java/io/acari/intro/StringBoutique.java
+++ src/main/java/io/acari/intro/StringBoutique.java
@@ -2,6 +2,15 @@ package io.acari.intro;
 
 public class StringBoutique {
 
-
+  /**
+   * A string is said to be beautiful if b occurs
+   * in it no more times than a;c occurs in it no more times than b; etc.
+   *
+   * @param inputString
+   * @return
+   */
+  boolean isBeautifulString(String inputString) {
+    return true;//gurl you always beautiful.
+  }
 
 }
