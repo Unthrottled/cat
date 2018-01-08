6e6468e2f3f9c2d6460a5600e863571a3b963e7d
DATBOI <alex@cyclicreference.space>
2018-01-08 11:53:33 -0600

    Test's are for winners!

---
+++ src/test/java/io/acari/intro/YouGotMailTest.java
@@ -0,0 +1,33 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class YouGotMailTest {
+  private final YouGotMail youGotMail = new YouGotMail();
+
+  @Test
+  public void findEmailDomainOne() throws Exception {
+    assertEquals("example.com", youGotMail.findEmailDomain("prettyandsimple@example.com"));
+  }
+
+  @Test
+  public void findEmailDomainTwo() throws Exception {
+    assertEquals("example.org", youGotMail.findEmailDomain("<>[]:,;@\\\"!#$%&*+-/=?^_{}| ~.a\\\"@example.org"));
+  }
+
+  @Test
+  public void findEmailDomainThree() throws Exception {
+    assertEquals("yandex.ru", youGotMail.findEmailDomain("someaddress@yandex.ru"));
+  }
+
+  @Test
+  public void findEmailDomainFour() throws Exception {
+    assertEquals("xample.org", youGotMail.findEmailDomain("\\\" \\\"@xample.org"));
+  }
+
+}
