a893fad35f6128f1ed85a918d8ed076ce7d2b79b
DATBOI <alexsimons9999@gmail.com>
2018-01-07 18:29:45 -0600

    some tests bouy

---
+++ src/test/java/io/acari/intro/MaxConsecutiveSumTest.java
@@ -0,0 +1,38 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxConsecutiveSumTest {
+    private final MaxConsecutiveSum maxConsecutiveSum = new MaxConsecutiveSum();
+
+    @Test
+    public void arrayMaxConsecutiveSumOne() throws Exception {
+        assertEquals(8,
+                maxConsecutiveSum.arrayMaxConsecutiveSum(new int[]{2, 3, 5, 1, 6},
+                        2));
+    }
+
+    @Test
+    public void arrayMaxConsecutiveSumTwo() throws Exception {
+        assertEquals(14,
+                maxConsecutiveSum.arrayMaxConsecutiveSum(new int[]{2, 4, 10, 1},
+                        2));
+    }
+
+    @Test
+    public void arrayMaxConsecutiveSumThree() throws Exception {
+        assertEquals(9,
+                maxConsecutiveSum.arrayMaxConsecutiveSum(new int[]{1, 3, 2, 4},
+                        3));
+    }
+
+    @Test
+    public void arrayMaxConsecutiveSumFour() throws Exception {
+        assertEquals(3,
+                maxConsecutiveSum.arrayMaxConsecutiveSum(new int[]{3, 2, 1, 1},
+                        1));
+    }
+
+}
