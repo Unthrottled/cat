a4a8910924f98edd92bfd4a525e51370efd4e0be
DATBOI <alexsimons9999@gmail.com>
2018-01-07 18:38:20 -0600

    too ez

--- src/main/java/io/acari/intro/MaxConsecutiveSum.java
+++ src/main/java/io/acari/intro/MaxConsecutiveSum.java
@@ -11,6 +11,17 @@ public class MaxConsecutiveSum {
      * @return
      */
     int arrayMaxConsecutiveSum(int[] inputArray, int k) {
-        return 0;
+        int max = Integer.MIN_VALUE;
+        int runningSum = 0;
+        for (int i = 1; i <= inputArray.length; i++) {
+            int currentIndex = i - 1;
+            int currentValue = inputArray[currentIndex];
+            runningSum += currentValue;
+            if(i >= k){
+                max = runningSum > max ? runningSum : max;
+                runningSum -= inputArray[currentIndex - k + 1];
+            }
+        }
+        return max;
     }
 }
