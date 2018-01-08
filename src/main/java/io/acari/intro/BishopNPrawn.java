57a5b18cf0f2147bbd36c1347f57d96e0082d933
DATBOI <alex@cyclicreference.space>
2018-01-08 10:06:04 -0600

    API Definition

--- src/main/java/io/acari/intro/BishopNPrawn.java
+++ src/main/java/io/acari/intro/BishopNPrawn.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class BishopNPrawn {
+
+  /**
+   * Given the positions of a
+   * white bishop and a black pawn on the standard
+   * chess board, determine whether the bishop can
+   * capture the pawn in one move.
+   *
+   * @param bishop
+   * @param pawn
+   * @return
+   */
+  boolean bishopAndPawn(String bishop, String pawn) {
+    return false;
+  }
 }
