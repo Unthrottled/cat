43405ec1ff0f6d688897915864abc830adb81668
DATBOI <alex@cyclicreference.space>
2018-01-08 11:48:59 -0600

    you wanted definition for the API, well here you go

--- src/main/java/io/acari/intro/YouGotMail.java
+++ src/main/java/io/acari/intro/YouGotMail.java
@@ -1,4 +1,17 @@
 package io.acari.intro;
 
 public class YouGotMail {
+
+  /**
+   * An email address such as "John.Smith@example.com"
+   * is made up of a local part ("John.Smith"),
+   * an "@" symbol, then a domain part ("example.com").
+   *
+   * @param address
+   * @return
+   */
+  String findEmailDomain(String address) {
+    return address;
+  }
+
 }
