1f11c75e3acc0f4e5aa9a94e343ac1197e278840
DATBOI <alexsimons9999@gmail.com>
2018-01-07 18:26:13 -0600

    new API

--- src/main/java/io/acari/intro/MaxConsecutiveSum.java
+++ src/main/java/io/acari/intro/MaxConsecutiveSum.java
@@ -1,4 +1,16 @@
 package io.acari.intro;
 
 public class MaxConsecutiveSum {
+
+    /**
+     * Given array of integers, find the maximal
+     * possible sum of some of its k consecutive elements.
+     *
+     * @param inputArray
+     * @param k
+     * @return
+     */
+    int arrayMaxConsecutiveSum(int[] inputArray, int k) {
+        return 0;
+    }
 }
