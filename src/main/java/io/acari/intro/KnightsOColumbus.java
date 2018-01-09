1e060895e2107a5fe43b2dc25b971d4a903ff495
DATBOI <alex@cyclicreference.space>
2018-01-09 08:31:00 -0600

    Now I know what to do

--- src/main/java/io/acari/intro/KnightsOColumbus.java
+++ src/main/java/io/acari/intro/KnightsOColumbus.java
@@ -1,4 +1,29 @@
 package io.acari.intro;
 
 public class KnightsOColumbus {
+
+
+  /**
+   * Given a position of a knight on the standard chessboard,
+   * find the number of different moves the knight can perform.
+   * <p>
+   * The knight can move to a square that is two squares
+   * horizontally and one square vertically, or two squares vertically
+   * and one square horizontally away from it.
+   * <p>
+   * The complete move
+   * therefore looks like the letter L.
+   * Check out the image below to see all
+   * valid moves for a knight piece that
+   * is placed on one of the central squares.
+   * <p>
+   * <img src="https://codefightsuserpics.s3.amazonaws.com/tasks/chessKnight/img/knight.jpg?_tm=1486560102464" alt="">
+   *
+   * @param cell
+   * @return
+   */
+  int chessKnight(String cell) {
+    return 0;
+  }
+
 }
