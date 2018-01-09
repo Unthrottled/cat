d574803b0fdb06464606b4b916b4813f10d625e1
DATBOI <alex@cyclicreference.space>
2018-01-09 15:24:46 -0600

    Class and API

---
+++ src/main/java/io/acari/intro/SumItUp.java
@@ -0,0 +1,27 @@
+package io.acari.intro;
+
+public class SumItUp {
+
+  /**
+   * CodeMaster has just returned from shopping.
+   * <p>
+   * He scanned the check of the items he bought and gave
+   * the resulting string to Ratiorg to figure out the
+   * total number of purchased items.
+   * <p>
+   * Since Ratiorg is a bot he is definitely going to automate it,
+   * so he needs a program that sums up all the numbers
+   * which appear in the given input.
+   * <p>
+   * Help Ratiorg by writing a
+   * function that returns the sum of numbers that
+   * appear in the given inputString.
+   *
+   * @param inputString
+   * @return
+   */
+  int sumUpNumbers(String inputString) {
+    return inputString.length();
+  }
+
+}
