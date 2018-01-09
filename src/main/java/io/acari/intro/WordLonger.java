87669ff1bdf71792b4914bccc46b8ea8663b3d84
DATBOI <alex@cyclicreference.space>
2018-01-09 11:53:22 -0600

    API definition

--- src/main/java/io/acari/intro/WordLonger.java
+++ src/main/java/io/acari/intro/WordLonger.java
@@ -1,4 +1,18 @@
 package io.acari.intro;
 
 public class WordLonger {
+
+  /**
+   * Define a word as a sequence of consecutive
+   * English letters.
+   * <p>
+   * Find the longest word from the given string.
+   *
+   * @param text
+   * @return
+   */
+  String longestWord(String text) {
+    return text;
+  }
+
 }
