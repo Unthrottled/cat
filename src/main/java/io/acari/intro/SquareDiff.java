3aaf86eff2016a56e88ddd01ce52f21b4843cc0c
DATBOI <alex@cyclicreference.space>
2018-01-09 15:45:13 -0600

    Class

---
+++ src/main/java/io/acari/intro/SquareDiff.java
@@ -0,0 +1,17 @@
+package io.acari.intro;
+
+public class SquareDiff {
+
+  /**
+   * Given a rectangular matrix containing
+   * only digits, calculate the number of
+   * different 2 × 2 squares in it.
+   *
+   * @param matrix
+   * @return
+   */
+  int differentSquares(int[][] matrix) {
+    return 0;
+  }
+
+}
