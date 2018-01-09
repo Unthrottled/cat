75e9da4b17021626fa27fd63ccc769c79c475039
DATBOI <alex@cyclicreference.space>
2018-01-09 14:59:49 -0600

    API definition

--- src/main/java/io/acari/intro/TimeValidation.java
+++ src/main/java/io/acari/intro/TimeValidation.java
@@ -1,4 +1,16 @@
 package io.acari.intro;
 
 public class TimeValidation {
+
+  /**
+   * Check if the given string is a correct
+   * time representation of the 24-hour clock.
+   *
+   * @param time
+   * @return
+   */
+  boolean validTime(String time) {
+    return false;
+  }
+
 }
