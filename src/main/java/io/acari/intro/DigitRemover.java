3315373be9b0d1a895bd36b6788fe9b3d664b8d1
DATBOI <alex@cyclicreference.space>
2018-01-09 10:03:19 -0600

    That's a API

--- src/main/java/io/acari/intro/DigitRemover.java
+++ src/main/java/io/acari/intro/DigitRemover.java
@@ -1,4 +1,17 @@
 package io.acari.intro;
 
 public class DigitRemover {
+
+  /**
+   * Given some integer, find the maximal number
+   * you can obtain by deleting exactly one digit
+   * of the given number.
+   *
+   * @param n
+   * @return
+   */
+  int deleteDigit(int n) {
+    return n;
+  }
+
 }
