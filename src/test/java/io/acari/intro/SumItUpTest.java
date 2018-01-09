e513ec3f91f15b7f8bbe4aee2c680353c2ee718a
DATBOI <alex@cyclicreference.space>
2018-01-09 15:29:41 -0600

    tests

---
+++ src/test/java/io/acari/intro/SumItUpTest.java
@@ -0,0 +1,34 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class SumItUpTest {
+  private final SumItUp sumItUp = new SumItUp();
+
+  @Test
+  public void sumUpNumbersOne() throws Exception {
+    assertEquals(14,
+        sumItUp.sumUpNumbers
+            ("2 apples, 12 oranges"));
+  }
+
+  @Test
+  public void sumUpNumbersTwo() throws Exception {
+    assertEquals(123450,
+        sumItUp.sumUpNumbers
+            ("123450"));
+  }
+
+  @Test
+  public void sumUpNumbersThree() throws Exception {
+    assertEquals(0,
+        sumItUp.sumUpNumbers
+            ("Your payment method is invalid"));
+  }
+
+}
