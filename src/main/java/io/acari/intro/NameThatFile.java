f5e2dd49c739d760355b1e0068bf23008d95374e
DATBOI <alex@cyclicreference.space>
2018-01-10 09:49:01 -0600

    dat api

--- src/main/java/io/acari/intro/NameThatFile.java
+++ src/main/java/io/acari/intro/NameThatFile.java
@@ -2,5 +2,22 @@ package io.acari.intro;
 
 public class NameThatFile {
 
+  /**
+   * You are given an array of desired filenames in the order
+   * of their creation.
+   * <p>
+   * Since two files cannot have equal names, the
+   * one which comes later will have an addition to its name in
+   * a form of (k), where k is the smallest positive integer such
+   * that the obtained name is not used yet.
+   * <p>
+   * Return an array of names that will be given to the files.
+   *
+   * @param names
+   * @return
+   */
+  String[] fileNaming(String[] names) {
+    return names;
+  }
 
 }
