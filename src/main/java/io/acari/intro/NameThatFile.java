3bebdb28d610a6dc08a3ef1bff91b4994eb0cd91
DATBOI <alex@cyclicreference.space>
2018-01-10 11:47:46 -0600

    parking!

--- src/main/java/io/acari/intro/NameThatFile.java
+++ src/main/java/io/acari/intro/NameThatFile.java
@@ -1,5 +1,10 @@
 package io.acari.intro;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class NameThatFile {
 
   /**
@@ -17,7 +22,64 @@ public class NameThatFile {
    * @return
    */
   String[] fileNaming(String[] names) {
+    int length = names.length;
+    Map<String, SortedSet<Integer>> fileNames = new HashMap<>(length);
+    for (int i = 0; i < length; i++) {
+      String current = names[i];
+      int lastOpenIndex = current.lastIndexOf('(');
+      int lastClosingIndex = current.lastIndexOf(')');
+      boolean validPostfix = lastOpenIndex > -1 && lastClosingIndex - lastOpenIndex > 0;
+      if (validPostfix) {
+        processPostFixedFileName(fileNames, current, lastOpenIndex, lastClosingIndex);
+      } else if(fileNames.containsKey(current)) {
+
+      } else {
+        //first time we have seen this!
+        fileNames.put(current, newSetPlusOne());
+      }
+    }
     return names;
   }
+
+  private void processPostFixedFileName(Map<String, SortedSet<Integer>> fileNames, String current, int lastOpenIndex, int lastClosingIndex) {
+    int number = -1;
+    boolean hasNumber = true;
+    try {
+      number = Integer.parseInt(current.substring(lastOpenIndex + 1, lastClosingIndex));
+    } catch (RuntimeException e) {
+      hasNumber = false;
+    }
+    if (hasNumber) {
+      processNumberedPostfix(fileNames, current, lastOpenIndex, number);
+    } else if (fileNames.containsKey(current)) {
+
+    } else {
+      //first time we have seen this!
+      fileNames.put(current, newSetPlusOne());
+    }
+  }
+
+  private void processNumberedPostfix(Map<String, SortedSet<Integer>> fileNames, String current, int lastOpenIndex, int number) {
+    String minusPostFix = current.substring(0, lastOpenIndex);
+    SortedSet<Integer> integers = fileNames.getOrDefault(minusPostFix, newSortedSet());
+    if(!integers.add(number)){
+      //The same! make a new one!
+      fileNames.put(current, newSetPlusOne());
+    } else {
+      //unique value!
+      fileNames.put(minusPostFix, integers);
+    }
+  }
+
+  private SortedSet<Integer> newSortedSet() {
+    return new TreeSet<>();
+  }
+
+  private SortedSet<Integer> newSetPlusOne() {
+    SortedSet<Integer> integers = newSortedSet();
+    integers.add(0);
+    return integers;
+  }
+
 
 }
