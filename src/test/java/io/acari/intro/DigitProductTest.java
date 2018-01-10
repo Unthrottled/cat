517aeef13051897d33b1572b7aa3b2aa398849e4
DATBOI <alex@cyclicreference.space>
2018-01-09 19:11:36 -0600

    testsgit add .

---
+++ src/test/java/io/acari/intro/DigitProductTest.java
@@ -0,0 +1,49 @@
+package io.acari.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class DigitProductTest {
+  private final DigitProduct digitProduct = new DigitProduct();
+
+  @Test
+  public void digitsProductOne() throws Exception {
+    assertEquals(26,
+        digitProduct.digitsProduct(12));
+  }
+
+  @Test
+  public void digitsProductTwo() throws Exception {
+    assertEquals(-1,
+        digitProduct.digitsProduct(19));
+  }
+
+  @Test
+  public void digitsProductThree() throws Exception {
+    assertEquals(2559,
+        digitProduct.digitsProduct(450));
+  }
+
+  @Test
+  public void digitsProductFour() throws Exception {
+    assertEquals(0,
+        digitProduct.digitsProduct(1));
+  }
+
+  @Test
+  public void digitsProductFive() throws Exception {
+    assertEquals(-1,
+        digitProduct.digitsProduct(13));
+  }
+
+  @Test
+  public void digitsProductSix() throws Exception {
+    assertEquals(1,
+        digitProduct.digitsProduct(1));
+  }
+
+}
