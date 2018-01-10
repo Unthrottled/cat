8d1d5ed682b53d979cbf2de57c3d226593028035
DATBOI <alex@cyclicreference.space>
2018-01-10 15:47:10 -0600

    api

--- src/main/java/io/acari/intro/BitsNPieces.java
+++ src/main/java/io/acari/intro/BitsNPieces.java
@@ -1,4 +1,26 @@
 package io.acari.intro;
 
 public class BitsNPieces {
+
+  /**
+   * You are taking part in an Escape Room challenge
+   * designed specifically for programmers.
+   * <p>
+   * In your efforts to find a clue, you've found a binary
+   * code written on the wall behind a vase, and realized that it
+   * must be an encrypted message.
+   * <p>
+   * After some thought, your first guess is that each
+   * consecutive 8 bits of the code stand for the character with
+   * the corresponding extended ASCII code.
+   * <p>
+   * Assuming that your hunch is correct, decode the message.
+   *
+   * @param code
+   * @return
+   */
+  String messageFromBinaryCode(String code) {
+    return code;
+  }
+
 }
