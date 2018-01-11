c6943008faa3aa5d9dd23e42196d2faffc036454
DATBOI <alex@cyclicreference.space>
2018-01-10 18:34:45 -0600

    API

--- src/main/java/io/acari/intro/Spirals.java
+++ src/main/java/io/acari/intro/Spirals.java
@@ -1,4 +1,17 @@
 package io.acari.intro;
 
 public class Spirals {
+
+  /**
+   * Construct a square matrix with a size N × N
+   * containing integers from 1 to N * N in a spiral
+   * order, starting from top-left and in clockwise
+   * direction.
+   *
+   * @param n
+   * @return
+   */
+  int[][] spiralNumbers(int n) {
+    return null;
+  }
 }
