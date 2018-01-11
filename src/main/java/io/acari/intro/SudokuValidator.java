62ce4ca679c5a11bfcea7aa566ab0051c0f1658e
DATBOI <alex@cyclicreference.space>
2018-01-11 10:07:37 -0600

    API definition

--- src/main/java/io/acari/intro/SudokuValidator.java
+++ src/main/java/io/acari/intro/SudokuValidator.java
@@ -1,4 +1,19 @@
 package io.acari.intro;
 
 public class SudokuValidator {
+
+  /**
+   * Sudoku is a number-placement puzzle.
+   * <p>
+   * The objective is to fill a 9 × 9 grid with digits
+   * so that each column, each row, and each of the nine
+   * 3 × 3 sub-grids that compose the grid contains all of
+   * the digits from 1 to 9.
+   *
+   * @param grid
+   * @return
+   */
+  boolean sudoku(int[][] grid) {
+    return false;
+  }
 }
