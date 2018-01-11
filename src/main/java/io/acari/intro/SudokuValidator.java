9e109cc8188486f1ec72a0c826fe0058fe4a6a0f
DATBOI <alex@cyclicreference.space>
2018-01-11 11:08:33 -0600

    very nice

--- src/main/java/io/acari/intro/SudokuValidator.java
+++ src/main/java/io/acari/intro/SudokuValidator.java
@@ -1,5 +1,8 @@
 package io.acari.intro;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SudokuValidator {
 
   /**
@@ -14,6 +17,75 @@ public class SudokuValidator {
    * @return
    */
   boolean sudoku(int[][] grid) {
+    return validateVertical(grid) &&
+        validateHorizontal(grid) &&
+        validateGroups(grid);
+  }
+
+  private boolean validateGroups(int[][] grid) {
+    for (int i = 0; i < grid.length; i += 3) {
+      for (int j = 0; j < grid.length; j += 3) {
+        if (isInvalidGroup(grid, i, j))
+          return false;
+      }
+    }
+
+    return true;
+  }
+
+  private boolean isInvalidGroup(int[][] grid, int i, int j) {
+    Set<Integer> set = new HashSet<>(9);
+    int i1 = i + 3;
+    int i2 = j + 3;
+    for (int k = i; k < i1; k++) {
+      for (int l = j; l < i2; l++) {
+        if (isInvalid(set, grid[k][l]))
+          return true;
+      }
+    }
     return false;
+  }
+
+  private boolean validateVertical(int[][] grid) {
+    for (int i = 0; i < grid.length; i++) {
+      if (inValidVertical(grid, i))
+        return false;
+    }
+    return true;
+  }
+
+  private boolean validateHorizontal(int[][] grid) {
+    for (int i = 0; i < grid[0].length; i++) {
+      if (inValidHorizontal(grid[i]))
+        return false;
+    }
+    return true;
+  }
+
+  private boolean inValidHorizontal(int[] i) {
+    Set<Integer> set = new HashSet<>(i.length);
+    for (int i1 : i) {
+      if (isInvalid(set, i1))
+        return true;
+    }
+    return badSize(i, set);
+  }
+
+  private boolean inValidVertical(int[][] i, int col) {
+    Set<Integer> set = new HashSet<>(i.length);
+    for (int j = 0; j < i.length; j++) {
+      int i1 = i[j][col];
+      if (isInvalid(set, i1))
+        return true;
+    }
+    return badSize(i[0], set);
+  }
+
+  private boolean badSize(int[] i, Set<Integer> set) {
+    return set.size() < i.length;
+  }
+
+  private boolean isInvalid(Set<Integer> set, int i1) {
+    return i1 < 1 || i1 > 9 || !set.add(i1);
   }
 }
--- src/test/java/io/acari/intro/SudokuValidatorTest.java
+++ src/test/java/io/acari/intro/SudokuValidatorTest.java
@@ -14,7 +14,7 @@ public class SudokuValidatorTest {
   @Test
   public void sudokuOne() throws Exception {
     assertTrue(sudokuValidator.sudoku(new int[][]
-        {{1, 3, 2, 5, 4, 6, 9, 8, 7},
+           {{1, 3, 2, 5, 4, 6, 9, 8, 7},
             {4, 6, 5, 8, 7, 9, 3, 2, 1},
             {7, 9, 8, 2, 1, 3, 6, 5, 4},
             {9, 2, 1, 4, 3, 5, 8, 7, 6},
@@ -44,7 +44,7 @@ public class SudokuValidatorTest {
   @Test
   public void sudokuThree() throws Exception {
     assertFalse(sudokuValidator.sudoku(new int[][]
-        {{1, 3, 4, 2, 5, 6, 9, 8, 7},
+           {{1, 3, 4, 2, 5, 6, 9, 8, 7},
             {4, 6, 8, 5, 7, 9, 3, 2, 1},
             {7, 9, 2, 8, 1, 3, 6, 5, 4},
             {9, 2, 3, 1, 4, 5, 8, 7, 6},
