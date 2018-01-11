a17d533f1da1ac1cc51c7c2a31d6023820f9985a
DATBOI <alex@cyclicreference.space>
2018-01-11 15:45:07 -0600

    Tests for the class

---
+++ src/test/java/io/acari/graph/KingdomRoadsTest.java
@@ -0,0 +1,49 @@
+package io.acari.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class KingdomRoadsTest {
+  private final KingdomRoads kingdomRoads = new KingdomRoads();
+
+  @Test
+  public void newRoadSystemOne() throws Exception {
+    assertTrue(kingdomRoads.newRoadSystem(new boolean[][]
+        {{false,true,false,false},
+ {false,false,true,false},
+ {true,false,false,true},
+ {false,false,true,false}}
+    ));
+  }
+
+  @Test
+  public void newRoadSystemTwo() throws Exception {
+    assertTrue(kingdomRoads.newRoadSystem(new boolean[][]
+        {{false,true,false,false,false,false,false},
+ {true,false,false,false,false,false,false},
+ {false,false,false,true,false,false,false},
+ {false,false,true,false,false,false,false},
+ {false,false,false,false,false,false,true},
+ {false,false,false,false,true,false,false},
+ {false,false,false,false,false,true,false}}
+    ));
+  }
+
+  @Test
+  public void newRoadSystemThree() throws Exception {
+    assertFalse(kingdomRoads.newRoadSystem(new boolean[][]
+        {{false,true,false,false,false,false,false},
+ {true,false,false,false,false,false,false},
+ {false,false,false,true,false,false,false},
+ {false,false,true,false,false,false,false},
+ {false,false,false,false,false,false,true},
+ {false,false,false,false,true,false,false},
+ {false,false,false,false,false,true,false}}
+    ));
+  }
+
+}
