69da4e7ae67e4377bb97eef8f9b5484f4978e863
DATBOI <alex@cyclicreference.space>
2018-01-12 13:39:35 -0600

    API Definition

--- src/main/java/io/acari/graph/ChangePlaces.java
+++ src/main/java/io/acari/graph/ChangePlaces.java
@@ -1,4 +1,33 @@
 package io.acari.graph;
 
 public class ChangePlaces {
+
+  /**
+   * Once upon a time, in a kingdom far, far away, there
+   * lived a king Byteasar VI. As any king with such a magnificent
+   * name, he was destined to leave a trace in history. Unfortunately
+   * imagination wasn't one of king Byteasar's strong suits, so the
+   * reform he came up with was quite simple: the king ordered to
+   * rename all the cities. He didn't want to come up with new names
+   * (as a king, he'd have to remember them all!), so he ordered the
+   * cities to swap their names in such manner that the ith city would
+   * have the name of the city number (i + 1)th. The last city in the
+   * Byteasar's kingdom would, naturally, get the name of the very first
+   * city.
+   * <p>
+   * The cartographers were not happy with this reform, since they had
+   * to redraw all the maps of the kingdom. Before the reform, information
+   * about all the roads between the cities were stored in the roadRegister.
+   * Prior to drawing maps, they had to start with updating the register.
+   * Your task is to find out what the updated register looked like.
+   * <p>
+   * The king is a dick.
+   *
+   * @param roadRegister
+   * @return
+   */
+  boolean[][] greatRenaming(boolean[][] roadRegister) {
+    return null;
+  }
+
 }
