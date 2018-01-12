e28964b8553ab7539b914d5ecb2cf6ccba38679e
DATBOI <alex@cyclicreference.space>
2018-01-12 11:16:30 -0600

    Api def

--- src/main/java/io/acari/graph/KingRoadEfficiency.java
+++ src/main/java/io/acari/graph/KingRoadEfficiency.java
@@ -1,4 +1,34 @@
 package io.acari.graph;
 
 public class KingRoadEfficiency {
+
+  /**
+   * Once upon a time, in a kingdom far, far away, there lived a
+   * king Byteasar III. As a smart and educated ruler, he did everything
+   * in his (unlimited) power to make every single system of his kingdom
+   * efficient. The king went down in history as a great road builder:
+   * during his reign a great number of roads were built, and the road
+   * system he created was the most efficient for those dark times.
+   * <p>
+   * When you started to learn about Byteasar's III deeds in your
+   * history classes, the creeping doubt crawled into the back of
+   * your mind: what if the famous king wasn't so great after all?
+   * According to the most recent studies, there were n cities in the
+   * Byteasar's kingdom, which were connected by the two-ways roads.
+   * You managed to get information about this roads from the university
+   * library, so now you can write a function that will determine whether
+   * the road system in Byteasar's kingdom was efficient or not.
+   * <p>
+   * A road system is considered efficient if it is possible to
+   * travel from any city to any other city by traversing at
+   * most 2 roads.
+   *
+   * @param n
+   * @param roads
+   * @return
+   */
+  boolean efficientRoadNetwork(int n, int[][] roads) {
+    return false;
+  }
+
 }
