572a160aa79d8b2ee019e94887e619745a8b17a9
DATBOI <alex@cyclicreference.space>
2018-01-12 13:01:01 -0600

    New class

--- src/main/java/io/acari/graph/KingdomLabels.java
+++ src/main/java/io/acari/graph/KingdomLabels.java
@@ -1,4 +1,41 @@
 package io.acari.graph;
 
 public class KingdomLabels {
+
+  /**
+   * Once upon a time, in a kingdom far, far away, there
+   * lived a king Byteasar V. His predecessor, king Byteasar IV,
+   * lived quite a long life, and when Byteasar V finally
+   * ascended the throne, he was already 150 years old.
+   * The new king had been preparing all his life for his
+   * moment of glory and, scared that he wouldn't have enough
+   * time to shine, started his reforms right away. T
+   * he first (and, as it turned out, the last) royal decree,
+   * issued within a couple of days after the coronation, ordered
+   * the following: all the road in the kingdom were to be named.
+   * <p>
+   * Unfortunately the king didn't have enough time to come up
+   * with actual names, so all the roads were to be names with
+   * numbers from 0 to roads.length - 1. As a born strategist,
+   * Byteasar wanted to make sure that the maps of his kingdom
+   * were confusing to enemies, which is why the road names were
+   * to be chosen so that no two roads with the neighboring names
+   * (i.e. names i and i + 1 for some i) would have a common end
+   * at one of the cities.
+   * <p>
+   * The archicartographer came up with the names for the roads,
+   * but he was not sure if the constraint the king imposed was met.
+   * He asked the Greater Power to help him check it.
+   * As a Greater Power from the future, you are the one
+   * who can help with that. Given the names for the roads the
+   * archicartographer came up with, check that no two roads with
+   * the neighboring names have a common end.
+   *
+   * @param roads
+   * @return
+   */
+  boolean namingRoads(int[][] roads) {
+    return false;
+  }
+
 }
