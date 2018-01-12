ed2088ba8b5db80fc0d73ac1a7cb4a68b1dfbf87
DATBOI <alex@cyclicreference.space>
2018-01-12 14:42:41 -0600

    API

--- src/main/java/io/acari/graph/ConquerTheCities.java
+++ src/main/java/io/acari/graph/ConquerTheCities.java
@@ -1,4 +1,30 @@
 package io.acari.graph;
 
 public class ConquerTheCities {
+
+  /**
+   * Once upon a time, in a kingdom far, far away, there lived
+   * a king Byteasar VII. He reigned during the Dark Times, so
+   * very little is known about those times. It is known that
+   * when he ascended the throne, there were n cities in the
+   * kingdom, connected by several two-way roads. But before the
+   * young king managed to start ruling, the enemy came to the gates:
+   * the evil emperor Bugoleon invaded the kingdom and started to
+   * conquer the cities day after day.
+   * <p>
+   * It is not known how long it took to capture each of
+   * the cities, but you've recently discovered an ancient
+   * chronicle saying that each day Bugoleon captured all the
+   * cities that had at most 1 neighboring free city at the moment.
+   * Knowing this fact, the number of cities n and all the roads of
+   * the kingdom, determine in how many days each of the cities was
+   * conquered.
+   *
+   * @param n
+   * @param roads
+   * @return
+   */
+  int[] citiesConquering(int n, int[][] roads) {
+    return null;
+  }
 }
