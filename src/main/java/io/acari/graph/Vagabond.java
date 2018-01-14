d2bf8039e8532e6a2d41dc0a40c718cbe94a6c06
DATBOI <alex@cyclicreference.space>
2018-01-14 13:47:20 -0600

    Call me what you will

--- src/main/java/io/acari/graph/Vagabond.java
+++ src/main/java/io/acari/graph/Vagabond.java
@@ -1,4 +1,34 @@
 package io.acari.graph;
 
 public class Vagabond {
+
+  /**
+   * In a kingdom far, far away, there lives a king Byteasar IX.
+   * The history of the kingdom is rich with events and actions,
+   * most of which has something to do with the cities of that kingdom.
+   * King Byteasar doesn't want to go down in history, and, most of all,
+   * he doesn't want to have anything to do with the cities of the kingdom.
+   * Cities are lame!
+   * <p>
+   * Instead, king Byteasar wants to focus on the roads,
+   * which is why he issued a new decree: from now on, all roads are
+   * considered to be cities, and all cities are considered to be roads.
+   * Now his gratefuller subjects pack their livings and move out from
+   * the cities to the roads, and the cartographers are busy with building
+   * a new roadRegister of the kingdom. All roads of the kingdom are to be
+   * named by the cities they connect (i.e. [city1, city2], city1 < city2),
+   * sorted lexicographically and enumerated starting from 0.
+   * A new road register for such a system should be created.
+   * <p>
+   * Your task is to help the cartographers with building the new
+   * road register. Handle the challenge, and the glorious kingdom of
+   * Byteasar IX will never have to deal with the tasks related to cities
+   * ever again!
+   *
+   * @param roadRegister
+   * @return
+   */
+  boolean[][] livingOnTheRoads(boolean[][] roadRegister) {
+    return roadRegister;
+  }
 }
