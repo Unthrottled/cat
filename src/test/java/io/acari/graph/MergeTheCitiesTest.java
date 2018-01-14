d07e08cb509f0d02b1f1a5c325b4849cd42111e9
DATBOI <alex@cyclicreference.space>
2018-01-14 10:53:17 -0600

    look at them tests!

---
+++ src/test/java/io/acari/graph/MergeTheCitiesTest.java
@@ -0,0 +1,66 @@
+package io.acari.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class MergeTheCitiesTest {
+  private final MergeTheCities mergeTheCities = new MergeTheCities();
+
+  @Test
+  public void mergingCitiesOne() throws Exception {
+    assertArrayEquals(new boolean[][]
+            {{false, true, true, false, true},
+                {true, false, false, true, true},
+                {true, false, false, false, false},
+                {false, true, false, false, false},
+                {true, true, false, false, false}}
+        ,
+        mergeTheCities.mergingCities(new boolean[][]
+            {{false, true, true, false, false, false, true},
+                {true, false, true, false, true, false, false},
+                {true, true, false, true, false, false, true},
+                {false, false, true, false, false, true, true},
+                {false, true, false, false, false, false, false},
+                {false, false, false, true, false, false, false},
+                {true, false, true, true, false, false, false}}
+        )
+    );
+  }
+
+  @Test
+  public void mergingCitiesTwo() throws Exception {
+    assertArrayEquals(new boolean[][]
+            {{false, false, false},
+                {false, false, false},
+                {false, false, false}}
+
+        ,
+        mergeTheCities.mergingCities(new boolean[][]
+            {{false, false, false},
+                {false, false, false},
+                {false, false, false}}
+
+        )
+    );
+  }
+
+  @Test
+  public void mergingCitiesSix() throws Exception {
+    assertArrayEquals(new boolean[][]
+            {{false, true, true},
+                {true, false, true},
+                {true, true, false}}
+
+        ,
+        mergeTheCities.mergingCities(new boolean[][]
+            {{false, true},
+                {true, false}}
+        )
+    );
+  }
+
+}
