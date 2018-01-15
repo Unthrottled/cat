cf2f60ad8d0f1a8c83c7f6b98996696e57054838
DATBOI <alex@cyclicreference.space>
2018-01-15 15:11:28 -0600

    what I am supposed to do

--- src/main/java/io/acari/graph/DasBook.java
+++ src/main/java/io/acari/graph/DasBook.java
@@ -1,4 +1,28 @@
 package io.acari.graph;
 
 public class DasBook {
+
+  /**
+   * You decided to create an automatic image recognizer that
+   * will determine the object in the image based on its contours.
+   * The main recognition system is already implemented, and
+   * now you need to start the teaching process.
+   * <p>
+   * Today you want to teach your program to recognize book patterns,
+   * which means that you need to implement a function that, given
+   * the adjacency matrix representing the contour,
+   * will determine whether it's a book or not.
+   * <p>
+   * A book consists of a base and may have any number of pages.
+   * <p>
+   * The book's base consists of a single edge connecting two nodes,
+   * and it is a common edge for all the pages. Besides that, every page
+   * has only one node connected to both sides of the base.
+   *
+   * @param adj
+   * @return
+   */
+  boolean isBook(boolean[][] adj) {
+    return false;
+  }
 }
