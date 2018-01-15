449e8f16555aa3d3fa41532c544d77287522340f
DATBOI <alex@cyclicreference.space>
2018-01-15 13:43:46 -0600

    new test

--- src/test/java/io/acari/graph/StarsTest.java
+++ src/test/java/io/acari/graph/StarsTest.java
@@ -91,4 +91,21 @@ public class StarsTest {
         )
     );
   }
+
+  @Test
+  public void countStarsFive() {
+    assertEquals(3,
+        stars.countStars(new boolean[][]
+            {{false,false,false,false,false,true,false,true,false}, 
+ {false,false,false,false,false,false,false,false,true}, 
+ {false,false,false,true,false,false,true,false,false}, 
+ {false,false,true,false,false,false,false,false,false}, 
+ {false,false,false,false,false,false,false,false,true}, 
+ {true,false,false,false,false,false,false,false,false}, 
+ {false,false,true,false,false,false,false,false,false}, 
+ {true,false,false,false,false,false,false,false,false}, 
+ {false,true,false,false,true,false,false,false,false}}
+        )
+    );
+  }
 }
