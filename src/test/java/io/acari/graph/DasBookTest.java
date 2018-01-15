430a3f11cfebeb81a8e5bb92e5c0eeb50cd94577
DATBOI <alex@cyclicreference.space>
2018-01-15 15:16:50 -0600

    tests for the best

---
+++ src/test/java/io/acari/graph/DasBookTest.java
@@ -0,0 +1,69 @@
+package io.acari.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class DasBookTest {
+  private final DasBook dasBook = new DasBook();
+
+  @Test
+  public void isBookOne() {
+    assertTrue(dasBook.isBook(new boolean[][]
+        {{false, true, true, true},
+            {true, false, true, true},
+            {true, true, false, false},
+            {true, true, false, false}}
+    ));
+  }
+
+  @Test
+  public void isBookTwo() {
+    assertTrue(dasBook.isBook(new boolean[][]
+        {{false, true},
+            {true, false}}
+    ));
+  }
+
+  @Test
+  public void isBookThree() {
+    assertFalse(dasBook.isBook(new boolean[][]
+        {{false, false, true, true, true},
+            {false, false, false, true, true},
+            {true, false, false, true, true},
+            {true, true, true, false, true},
+            {true, true, true, true, false}}
+    ));
+  }
+
+  @Test
+  public void isBookFour() {
+    assertTrue(dasBook.isBook(new boolean[][]
+        {{false, true, false, false, false, false, true, false, false, false},
+            {true, false, true, true, true, true, true, true, true, true},
+            {false, true, false, false, false, false, true, false, false, false},
+            {false, true, false, false, false, false, true, false, false, false},
+            {false, true, false, false, false, false, true, false, false, false},
+            {false, true, false, false, false, false, true, false, false, false},
+            {true, true, true, true, true, true, false, true, true, true},
+            {false, true, false, false, false, false, true, false, false, false},
+            {false, true, false, false, false, false, true, false, false, false},
+            {false, true, false, false, false, false, true, false, false, false}}
+    ));
+  }
+
+  @Test
+  public void isBookFive() {
+    assertFalse(dasBook.isBook(new boolean[][]
+        {{false, false, false, true, true},
+            {false, false, false, true, true},
+            {false, false, false, true, true},
+            {true, true, true, false, true},
+            {true, true, true, true, true}}
+    ));
+  }
+}
