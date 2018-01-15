30b796a95cc6d5a5b4184f6247f262ee4364e2ef
DATBOI <alex@cyclicreference.space>
2018-01-15 14:16:35 -0600

    AAAAYYYYPPEEEEEIIIIIIIIIII

--- src/main/java/io/acari/graph/Wheelie.java
+++ src/main/java/io/acari/graph/Wheelie.java
@@ -1,4 +1,28 @@
 package io.acari.graph;
 
 public class Wheelie {
+
+  /**
+   * You decided to create an automatic image recognizer
+   * that will determine the object in the image based on its contours.
+   * The main recognition system is already implemented,
+   * and now you need to start the teaching process.
+   * <p>
+   * Today you want to teach your program to recognize wheel patterns,
+   * which means that you need to implement a function that,
+   * given the adjacency matrix representing the contour,
+   * will determine whether it's a wheel or not.
+   * <p>
+   * The wheel contour can be thought of as a single center vertex
+   * and a regular polygon with n (n > 2) vertices
+   * which are all connected to the center.
+   *
+   * @param adj
+   * @return
+   */
+  boolean isWheel(boolean[][] adj) {
+    return false;
+  }
+
+
 }
