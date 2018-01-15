90dc04e4fd75aa5361799bacd35179c52f0297fe
DATBOI <alex@cyclicreference.space>
2018-01-15 12:36:50 -0600

    tests

---
+++ src/test/java/io/acari/graph/StarsTest.java
@@ -0,0 +1,67 @@
+package io.acari.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class StarsTest {
+  private final Stars stars = new Stars();
+
+  @Test
+  public void countStarsOne() {
+    assertEquals(2,
+        stars.countStars(new boolean[][]
+            {{false, true, false, false, false},
+                {true, false, false, false, false},
+                {false, false, false, true, false},
+                {false, false, true, false, false},
+                {false, false, false, false, false}}
+        )
+    );
+  }
+
+  @Test
+  public void countStarsTwo() {
+    assertEquals(2,
+        stars.countStars(new boolean[][]
+            {{false, true, false, false, false},
+                {true, false, false, false, false},
+                {false, false, false, true, false},
+                {false, false, true, false, false},
+                {false, false, false, false, true}}
+        )
+    );
+  }
+
+  @Test
+  public void countStarsThree() {
+    assertEquals(0,
+        stars.countStars(new boolean[][]
+            {{false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, false, false, false, false},
+                {false, false, false, false, false}}
+        )
+    );
+  }
+
+  @Test
+  public void countStarsFour() {
+    assertEquals(1,
+        stars.countStars(new boolean[][]
+            {{false, true, true, true, true, true, true, true},
+                {true, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false}}
+        )
+    );
+  }
+}
