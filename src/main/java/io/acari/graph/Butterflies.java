7799b2df3bc5786b0b5a8755a13b6102f9eccd8e
DATBOI <alex@cyclicreference.space>
2018-01-15 10:51:06 -0600

    api

--- src/main/java/io/acari/graph/Butterflies.java
+++ src/main/java/io/acari/graph/Butterflies.java
@@ -1,4 +1,29 @@
 package io.acari.graph;
 
 public class Butterflies {
+
+  /**
+   * You decided to create an automatic image recognizer
+   * that will determine the object in the image based on its contours.
+   * The main recognition system is already implemented, and now you need
+   * to start the teaching process.
+   * <p>
+   * Today you want to teach your program to recognize butterfly patterns,
+   * which means that you need to implement a function that, given the
+   * adjacency matrix representing the contour, will determine whether it's a butterfly or not.
+   * <p>
+   * The butterfly contour looks like this:
+   * <p>
+   * <img src="https://codefightsuserpics.s3.amazonaws.com/tasks/isButterfly/img/butterfly.png?_tm=1495120253756"/>
+   * <p>
+   * Given the object's contour as an undirected graph
+   * represented by adjacency matrix adj determine whether it's a butterfly or not.
+   *
+   * @param adj
+   * @return
+   */
+  boolean isButterfly(boolean[][] adj) {
+    return false;
+  }
+
 }
