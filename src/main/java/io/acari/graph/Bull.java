69a28174e52e5807888ba6da80f14b939bb1878a
DATBOI <alex@cyclicreference.space>
2018-01-16 16:18:52 -0600

    API

--- src/main/java/io/acari/graph/Bull.java
+++ src/main/java/io/acari/graph/Bull.java
@@ -1,4 +1,27 @@
 package io.acari.graph;
 
 public class Bull {
+
+  /**
+   * You decided to create an automatic image recognizer that will
+   * determine the object in the image based on its contours. The main
+   * recognition system is already implemented, and now
+   * you need to start the teaching process.
+   *
+   * Today you want to teach your program to recognize bull
+   * patterns, which means that you need to implement a function
+   * that, given the adjacency matrix representing the contour, will
+   * determine whether it's a bull or not.
+   *
+   * You noticed that although all bull heads are similar
+   * there is some variation in the shapes of their horns, so there
+   * are several possible bull contours. The image below shows all such contours.
+   *
+   * @param adj
+   * @return
+   */
+  boolean isBull(boolean[][] adj) {
+    return false;
+  }
+
 }
