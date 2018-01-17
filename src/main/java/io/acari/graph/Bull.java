47d4fb8f150b1270de1f388454e5e1b10d0a4be2
DATBOI <alex@cyclicreference.space>
2018-01-17 09:07:57 -0600

    parking

--- src/main/java/io/acari/graph/Bull.java
+++ src/main/java/io/acari/graph/Bull.java
@@ -1,8 +1,6 @@
 package io.acari.graph;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,12 +11,12 @@ public class Bull {
    * determine the object in the image based on its contours. The main
    * recognition system is already implemented, and now
    * you need to start the teaching process.
-   *
+   * <p>
    * Today you want to teach your program to recognize bull
    * patterns, which means that you need to implement a function
    * that, given the adjacency matrix representing the contour, will
    * determine whether it's a bull or not.
-   *
+   * <p>
    * You noticed that although all bull heads are similar
    * there is some variation in the shapes of their horns, so there
    * are several possible bull contours. The image below shows all such contours.
@@ -27,19 +25,74 @@ public class Bull {
    * @return
    */
   boolean isBull(boolean[][] adj) {
+    int length = adj.length;
+    for (int i = 0; i < length; i++) {
+      if (adj[i][i])
+        return false;
+    }
+
+    Map<Integer, Node> cityGraph = createCityGraph(adj);
+    Queue<Node> queue = new LinkedList<>();
+    Set<Node> visited = new HashSet<>();
+    queue.offer(cityGraph.get(0));
+    while (!queue.isEmpty()) {
+      Node current = queue.poll();
+      visited.add(current);
+      if (isPartOfHead(current)) {
+        return hasHorns(current);
+      } else {
+        current.neighbors.stream()
+            .filter(n -> !visited.contains(n))
+            .forEach(queue::offer);
+      }
+    }
+
     return false;
   }
 
-  private Map<String, Node> createCityGraph(boolean[][] roadRegister, int length) {
-    Map<String, Node> graph = IntStream.range(0, length)
-            .boxed()
-            .map(Object::toString)
-            .collect(Collectors.toMap(a -> a, Node::new));
+  private boolean hasHorns(Node current) {
+    for (Node neighbor : current.neighbors) {
+      for (Node otherNeighbor : neighbor.neighbors) {
+        if (otherNeighbor.isConnected(current)) {
+          long singleHorns =
+              countSingleHorns(otherNeighbor, current, neighbor) +
+                  countSingleHorns(current, otherNeighbor, neighbor) +
+                  countSingleHorns(current, neighbor, otherNeighbor);
+          return singleHorns == 2;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private long countSingleHorns(Node current, Node neighbor, Node otherNeighbor) {
+    return neighbor.neighbors.stream()
+        .filter(n -> !current.equals(n))
+        .filter(n -> !otherNeighbor.equals(n))
+        .filter(n -> n.neighbors.size() == 1)
+        .filter(n -> n.neighbors.contains(neighbor))
+        .count();
+  }
+
+  private boolean isPartOfHead(Node current) {
+    return current.neighbors.stream()
+        .filter(neighbor -> neighbor.neighbors.stream()
+            .filter(otherNeighbor -> otherNeighbor.isConnected(current))
+            .count() == 1)
+        .count() == 1;
+  }
+
+  private Map<Integer, Node> createCityGraph(boolean[][] roadRegister) {
+    int length = roadRegister.length;
+    Map<Integer, Node> graph = IntStream.range(0, length)
+        .boxed()
+        .collect(Collectors.toMap(a -> a, Node::new));
     for (int i = 0; i < length; i++) {
-      Node one = graph.get(String.valueOf(i));
+      Node one = graph.get(i);
       for (int j = 0; j < length; j++) {
         if (roadRegister[i][j]) {
-          Node two = graph.get(String.valueOf(j));
+          Node two = graph.get(j);
           one.addNeighbor(two);
         }
       }
@@ -48,10 +101,10 @@ public class Bull {
   }
 
   class Node {
-    final String number;
+    final Integer number;
     final Set<Node> neighbors;
 
-    public Node(String number) {
+    public Node(Integer number) {
       this.number = number;
       this.neighbors = new HashSet<>();
     }
@@ -82,8 +135,8 @@ public class Bull {
     @Override
     public String toString() {
       return "Node{" +
-              "number='" + number + '\'' +
-              '}';
+          "number='" + number + '\'' +
+          '}';
     }
   }
 }
