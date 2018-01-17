201128aa3bc00555facab4397d2df4d1029d1121
DATBOI <alex@cyclicreference.space>
2018-01-17 09:47:31 -0600

    api todo

--- src/main/java/io/acari/graph/TadPole.java
+++ src/main/java/io/acari/graph/TadPole.java
@@ -1,4 +1,27 @@
 package io.acari.graph;
 
 public class TadPole {
+
+  /**
+   * You decided to create an automatic image recognizer that
+   * will determine the object in the image based on its contours.
+   * The main recognition system is already implemented, and now
+   * you need to start the teaching process.
+   * <p>
+   * Today you want to teach your program to recognize tadpole
+   * patterns, which means that you need to implement a function
+   * that, given the adjacency matrix representing the contour,
+   * will determine whether it's a tadpole or not.
+   * <p>
+   * The tadpole contour consists of two parts: a head which is
+   * a cycle with n (n > 2) vertices, and a tail which is a simple
+   * path (with at least one vertex) connected to the head.
+   *
+   * @param adj
+   * @return
+   */
+  boolean isTadpole(boolean[][] adj) {
+    return false;
+  }
+
 }
