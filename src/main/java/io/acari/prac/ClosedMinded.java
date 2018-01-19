126e442930c520bc35a7c10fef2ef4f0b9136335
DATBOI <alex@cyclicreference.space>
2018-01-19 11:46:27 -0600

    api def

--- src/main/java/io/acari/prac/ClosedMinded.java
+++ src/main/java/io/acari/prac/ClosedMinded.java
@@ -1,4 +1,19 @@
 package io.acari.prac;
 
 public class ClosedMinded {
+
+  /**
+   * Given an array of integers nums and an integer k,
+   * determine whether there are two distinct indices i and j
+   * in the array where nums[i] = nums[j] and
+   * the absolute difference between i and j is less than or equal to k.
+   *
+   * @param nums
+   * @param k
+   * @return
+   */
+  boolean containsCloseNums(int[] nums, int k) {
+    return false;
+  }
+
 }
