16f7bc88c14abe26e486afa48e0fbf4b0bc1513d
DATBOI <alex@cyclicreference.space>
2018-01-19 12:47:28 -0600

    It's probably around Seven...Thousand

--- src/main/java/io/acari/prac/DoYouKnowDaSums.java
+++ src/main/java/io/acari/prac/DoYouKnowDaSums.java
@@ -1,4 +1,18 @@
 package io.acari.prac;
 
 public class DoYouKnowDaSums {
+
+  /**
+   * You have a collection of coins, and you know the values
+   * of the coins and the quantity of each type of coin in it.
+   * You want to know how many distinct sums you can make
+   * from non-empty groupings of these coins.
+   *
+   * @param coins
+   * @param quantity
+   * @return
+   */
+  int possibleSums(int[] coins, int[] quantity) {
+    return 0;
+  }
 }
