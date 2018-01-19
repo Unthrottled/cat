89b1ee45067c77849ebfd2007b5c6778c559e0f1
DATBOI <alex@cyclicreference.space>
2018-01-19 14:34:12 -0600

    lul wut?

--- src/main/java/io/acari/prac/DoYouKnowDaSums.java
+++ src/main/java/io/acari/prac/DoYouKnowDaSums.java
@@ -12,44 +12,16 @@ public class DoYouKnowDaSums {
    * from non-empty groupings of these coins.
    *
    * @param coins
-   * @param quantity
+   * @param quantityOfEachCoin
    * @return
    */
-  int possibleSums(int[] coins, int[] quantity) {
+  int possibleSums(int[] coins, int[] quantityOfEachCoin) {
     Set<Integer> visited = new HashSet<>();
     Map<Integer, Set<Integer>> cache = new HashMap<>();
     int daWaes = 0;
-    for (int q : quantity) {
-      if (visited.add(q)) {
-        findDaWaes(coins, q, cache);
-        daWaes = q;
-      }
-    }
+
 
     return cache.get(daWaes).size();
   }
 
-  private void findDaWaes(int[] coins, int q, Map<Integer, Set<Integer>> cache) {
-    if (q == 0 || cache.containsKey(q)) {
-      return;
-    }
-
-    if (q == 1) {
-      cache.put(q, Arrays.stream(coins).boxed().collect(Collectors.toSet()));
-    } else {
-      findDaWaes(coins, q - 1, cache);
-      cacheDaWae(coins, q, cache);
-    }
-
-  }
-
-  private void cacheDaWae(int[] coins, int q, Map<Integer, Set<Integer>> cache) {
-    Set<Integer> previousSet = cache.get(q - 1);
-    Set<Integer> newSet = new HashSet<>(previousSet);
-    for (int i = 0; i < q; i++) {
-
-    }
-
-    cache.put(q, newSet);
-  }
 }
