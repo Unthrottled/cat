8d70e8cf5c25102ecac1a30c38570159e59b958e
DATBOI <alex@cyclicreference.space>
2018-01-19 14:43:42 -0600

    API Def

--- src/main/java/io/acari/prac/GivenSum.java
+++ src/main/java/io/acari/prac/GivenSum.java
@@ -1,4 +1,26 @@
 package io.acari.prac;
 
 public class GivenSum {
+
+  /**
+   * Given a binary tree t and an integer s,
+   * determine whether there is a root to leaf path
+   * in t such that the sum of vertex values equals s.
+   *
+   * @param t
+   * @param s
+   * @return
+   */
+  boolean hasPathWithGivenSum(Tree<Integer> t, int s) {
+    return false;
+  }
+
+  class Tree<T> {
+    T value;
+    Tree<T> left;
+    Tree<T> right;
+    Tree(T x) {
+      value = x;
+    }
+  }
 }
