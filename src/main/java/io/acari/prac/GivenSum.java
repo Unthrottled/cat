dbf32fb5d3a7e152f5bdcaca6e0ab9ab6821a09f
DATBOI <alex@cyclicreference.space>
2018-01-19 14:52:42 -0600

    baller

--- src/main/java/io/acari/prac/GivenSum.java
+++ src/main/java/io/acari/prac/GivenSum.java
@@ -12,7 +12,21 @@ public class GivenSum {
    * @return
    */
   boolean hasPathWithGivenSum(Tree<Integer> t, int s) {
-    return false;
+    return helper(t, s, 0);
+  }
+
+  private boolean helper(Tree<Integer> t, int s, int sum){
+    if(t == null){
+      return s == 0;
+    } else {
+      Integer value = t.value;
+      int rootPlus = sum + value;
+      if(t.left == null && t.right == null){
+        return s == rootPlus;
+      } else {
+        return helper(t.left, s, rootPlus) || helper(t.right, s, rootPlus);
+      }
+    }
   }
 
   class Tree<T> {
