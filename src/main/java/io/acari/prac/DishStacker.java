0a1635d1304f523cb0983bbb87a4b8e053ff403a
DATBOI <alex@cyclicreference.space>
2018-01-19 11:26:21 -0600

    one and dun

--- src/main/java/io/acari/prac/DishStacker.java
+++ src/main/java/io/acari/prac/DishStacker.java
@@ -1,5 +1,8 @@
 package io.acari.prac;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class DishStacker {
 
   /**
@@ -20,8 +23,45 @@ public class DishStacker {
    * @return
    */
   String[][] groupingDishes(String[][] dishes) {
-    return dishes;
+    SortedMap<String, SortedSet<String>> grouped = Arrays.stream(dishes)
+        .flatMap(dish -> Arrays.stream(dish)
+            .skip(1)
+            .map(i -> new Edge<>(dish[0], i)))
+        .collect(Collectors.groupingBy(p -> p.name, TreeMap::new, Collectors.mapping(p -> p.fst, Collectors.toCollection(TreeSet::new))));
+    int count = (int) grouped.entrySet().stream()
+        .map(Map.Entry::getValue)
+        .map(Set::size)
+        .filter(s -> s > 1)
+        .count();
+    String[][] newReturn = new String[count][];
+    int index = 0;
+    for (Map.Entry<String, SortedSet<String>> stringSortedSetEntry : grouped.entrySet()) {
+      if (stringSortedSetEntry.getValue().size() > 1) {
+        newReturn[index++] = toArray(stringSortedSetEntry);
+      }
+    }
+    return newReturn;
   }
 
+  private String[] toArray(Map.Entry<String, SortedSet<String>> entry) {
+    String ingredient = entry.getKey();
+    SortedSet<String> dishes = entry.getValue();
+    String[] returnValue = new String[dishes.size() + 1];
+    int index = 0;
+    returnValue[index++] = ingredient;
+    for (String dish : dishes) {
+      returnValue[index++] = dish;
+    }
+    return returnValue;
+  }
 
+  class Edge<A, B> {
+    final A fst;
+    final B name;
+
+    Edge(A var1, B name) {
+      this.fst = var1;
+      this.name = name;
+    }
+  }
 }
