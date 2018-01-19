eb42235f48a022db3c9bfab3f1668c70ef66f2bc
DATBOI <alex@cyclicreference.space>
2018-01-19 12:56:44 -0600

    Test's are done, and so are the references to the songs that i am listening to

---
+++ src/test/java/io/acari/prac/DoYouKnowDaSumsTest.java
@@ -0,0 +1,39 @@
+package io.acari.prac;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class DoYouKnowDaSumsTest {
+  private final DoYouKnowDaSums doYouKnowDaSums = new DoYouKnowDaSums();
+
+  @Test
+  public void possibleSumsOne() {
+    assertEquals(9, doYouKnowDaSums.possibleSums(new int[]
+            {10, 50, 100}
+        , new int[]
+            {1, 2, 1}
+    ));
+  }
+
+  @Test
+  public void possibleSumsTwo() {
+    assertEquals(122, doYouKnowDaSums.possibleSums(new int[]
+            {10, 50, 100, 500}
+        , new int[]
+            {5, 3, 2, 2}
+    ));
+  }
+
+  @Test
+  public void possibleSumsThree() {
+    assertEquals(5, doYouKnowDaSums.possibleSums(new int[]
+            {1}
+        , new int[]
+            {5}
+    ));
+  }
+}
