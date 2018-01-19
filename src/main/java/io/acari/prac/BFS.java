09c4e3c59ede4c6e380340c131236a823706621e
DATBOI <alex@cyclicreference.space>
2018-01-19 15:41:26 -0600

    new class

---
+++ src/main/java/io/acari/prac/BFS.java
@@ -0,0 +1,18 @@
+package io.acari.prac;
+
+public class BFS {
+
+
+  int[] traverseTree(Tree<Integer> t) {
+    return null;
+  }
+
+  class Tree<T> {
+    T value;
+    Tree<T> left;
+    Tree<T> right;
+    Tree(T x) {
+      value = x;
+    }
+  }
+}
