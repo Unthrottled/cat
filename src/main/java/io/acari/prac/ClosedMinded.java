1d0edeff473fca39c7e7384f2d1251e745bd5e3e
DATBOI <alex@cyclicreference.space>
2018-01-19 12:44:40 -0600

    Old people burning, Old People Burning

--- src/main/java/io/acari/prac/ClosedMinded.java
+++ src/main/java/io/acari/prac/ClosedMinded.java
@@ -1,5 +1,10 @@
 package io.acari.prac;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ClosedMinded {
 
   /**
@@ -13,7 +18,29 @@ public class ClosedMinded {
    * @return
    */
   boolean containsCloseNums(int[] nums, int k) {
-    return false;
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+      List<Integer> list = map.getOrDefault(num, new ArrayList<>());
+      list.add(i);
+      map.put(num, list);
+    }
+
+    return map.entrySet()
+        .stream()
+        .filter(e->e.getValue().size() >1)
+        .map(Map.Entry::getValue)
+        .anyMatch(l->{
+          for (int i = 0; i < l.size() - 1; i++) {
+            int one = l.get(i) + 1;
+            for (int j = i + 1; j < l.size(); j++) {
+                int two = l.get(j) + 1;
+                if(two - one <= k)
+                  return true;
+            }
+          }
+          return false;
+        });
   }
 
 }
