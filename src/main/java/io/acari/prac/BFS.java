4b431bd8613a014ea13f61dcc3bbfb5f03f797f0
DATBOI <alex@cyclicreference.space>
2018-01-19 15:50:19 -0600

    neat!

--- src/main/java/io/acari/prac/BFS.java
+++ src/main/java/io/acari/prac/BFS.java
@@ -1,10 +1,34 @@
 package io.acari.prac;
 
+import java.util.*;
+
 public class BFS {
 
 
   int[] traverseTree(Tree<Integer> t) {
-    return null;
+    List<Integer> nodesInOrder = new ArrayList<>();
+    if(t != null){
+      Set<Tree<Integer>> visited = new HashSet<>();
+      Queue<Tree<Integer>> queue = new LinkedList<>();
+      queue.offer(t);
+      while (!queue.isEmpty()){
+        Tree<Integer> current = queue.poll();
+        nodesInOrder.add(current.value);
+        visited.add(current);
+        if(current.left != null && !visited.contains(current.left)){
+          queue.offer(current.left);
+        }
+        if(current.right != null && !visited.contains(current.right)){
+          queue.offer(current.right);
+        }
+      }
+    }
+
+    int[] returnGuy = new int[nodesInOrder.size()];
+    for (int i = 0; i < nodesInOrder.size(); i++) {
+      returnGuy[i] = nodesInOrder.get(i);
+    }
+    return returnGuy;
   }
 
   class Tree<T> {
