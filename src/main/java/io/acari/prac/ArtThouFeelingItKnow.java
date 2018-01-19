5af86a04462ad3944c48ca2af33d8ff686ced2fc
DATBOI <alex@cyclicreference.space>
2018-01-19 11:28:55 -0600

    API definition

--- src/main/java/io/acari/prac/ArtThouFeelingItKnow.java
+++ src/main/java/io/acari/prac/ArtThouFeelingItKnow.java
@@ -1,4 +1,20 @@
 package io.acari.prac;
 
 public class ArtThouFeelingItKnow {
+
+  /**
+   * Given an array strings, determine whether it follows the sequence given
+   * in the patterns array. In other words, there should be no i and j for
+   * which strings[i] = strings[j] and patterns[i] ≠ patterns[j] or
+   * for which strings[i] ≠ strings[j] and patterns[i] = patterns[j].
+   *
+   * @param strings
+   * @param patterns
+   * @return
+   */
+  boolean areFollowingPatterns(String[] strings, String[] patterns) {
+    return false;
+  }
+
+
 }
