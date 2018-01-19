8e9e47c8eefb2c86627402bcf7134a8e6a6a5299
DATBOI <alex@cyclicreference.space>
2018-01-19 10:40:12 -0600

    API Def

--- src/main/java/io/acari/prac/DishStacker.java
+++ src/main/java/io/acari/prac/DishStacker.java
@@ -1,4 +1,27 @@
 package io.acari.prac;
 
 public class DishStacker {
+
+  /**
+   * You have a list of dishes. Each dish is associated
+   * with a list of ingredients used to prepare it.
+   * You want to group the dishes by ingredients, so that for
+   * each ingredient you'll be able to find all the
+   * dishes that contain it (if there are at least 2 such dishes).
+   * <p>
+   * Return an array where each element is a list with
+   * the first element equal to the name of the ingredient
+   * and all of the other elements equal to the names of dishes that contain this ingredient.
+   * The dishes inside each list should be sorted lexicographically.
+   * The result array should be sorted lexicographically
+   * by the names of the ingredients in its elements.
+   *
+   * @param dishes
+   * @return
+   */
+  String[][] groupingDishes(String[][] dishes) {
+    return dishes;
+  }
+
+
 }
