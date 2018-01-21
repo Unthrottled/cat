f79c2a2a204c423b6ed08e374a51ca320b6c937c
DATBOI <alex@cyclicreference.space>
2018-01-20 18:01:32 -0600

    what needs to be done

--- src/main/java/io/acari/prac/FirstUniqueChar.java
+++ src/main/java/io/acari/prac/FirstUniqueChar.java
@@ -2,8 +2,20 @@ package io.acari.prac;
 
 public class FirstUniqueChar {
 
+  /**
+   * Note: Write a solution that only
+   * iterates over the string once and uses O(1) additional memory,
+   * since this is what you would be asked to do during a real interview.
+   * <p>
+   * Given a string s, find and return the first
+   * instance of a non-repeating character in it.
+   * If there is no such character, return '_'.
+   *
+   * @param s
+   * @return
+   */
   char firstNotRepeatingCharacter(String s) {
-    if(s.length() > 1){
+    if (s.length() > 1) {
 
     }
 
