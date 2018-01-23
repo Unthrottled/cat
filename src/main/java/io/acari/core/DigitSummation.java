e69b83264b9003b771c500e4d11645216effe957
DATBOI <alex@cyclicreference.space>
2018-01-23 11:19:55 -0600

    Api def

--- src/main/java/io/acari/core/DigitSummation.java
+++ src/main/java/io/acari/core/DigitSummation.java
@@ -1,4 +1,14 @@
 package io.acari.core;
 
 public class DigitSummation {
+
+  /**
+   * You are given a two-digit integer n. Return the sum of its digits.
+   *
+   * @param n
+   * @return
+   */
+  int addTwoDigits(int n) {
+    return 0;
+  }
 }
