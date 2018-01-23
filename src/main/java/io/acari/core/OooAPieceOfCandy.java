79b6c6d3a164793eeb55fa7433eb810d8832250b
DATBOI <alex@cyclicreference.space>
2018-01-23 11:35:51 -0600

    maff

--- src/main/java/io/acari/core/OooAPieceOfCandy.java
+++ src/main/java/io/acari/core/OooAPieceOfCandy.java
@@ -2,18 +2,18 @@ package io.acari.core;
 
 public class OooAPieceOfCandy {
   /**
-   * n children have got m pieces of candy.
+   * numberOfChildren children have got m pieces of candy.
    * They want to eat as much candy as they can, but each
    * child must eat exactly the same amount of candy as any other child.
    * Determine how many pieces of candy will be eaten by all the children together.
    * Individual pieces of candy cannot be split.
    *
-   * @param n
-   * @param m
+   * @param numberOfChildren
+   * @param candyCount
    * @return
    */
-  int candies(int n, int m) {
-    return 0;
+  int candies(int numberOfChildren, int candyCount) {
+    return candyCount - (candyCount%numberOfChildren);
   }
 
 }
