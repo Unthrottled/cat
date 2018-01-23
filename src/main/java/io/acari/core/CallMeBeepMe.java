675f90a0471f6da528a6dae5aa0f33c0ad125948
DATBOI <alex@cyclicreference.space>
2018-01-23 15:15:21 -0600

    API def

--- src/main/java/io/acari/core/CallMeBeepMe.java
+++ src/main/java/io/acari/core/CallMeBeepMe.java
@@ -1,4 +1,28 @@
 package io.acari.core;
 
 public class CallMeBeepMe {
+
+  /**
+   * Some phone usage rate may be described as follows:
+   * <p>
+   * first minute of a call costs min1 cents,
+   * <p>
+   * each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+   * <p>
+   * each minute after 10th costs min11 cents.
+   * <p>
+   * You have s cents on your account before the call. What is the duration
+   * of the longest call (in minutes rounded down to
+   * the nearest integer) you can have?
+   *
+   * @param min1
+   * @param min2_10
+   * @param min11
+   * @param s
+   * @return
+   */
+  int phoneCall(int min1, int min2_10, int min11, int s) {
+    return 0;
+  }
+
 }
