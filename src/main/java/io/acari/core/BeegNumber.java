a2c52c52dc880874ee908a928410b16dbb9f23aa
DATBOI <alex@cyclicreference.space>
2018-01-23 11:25:57 -0600

    Thing I need to do

--- src/main/java/io/acari/core/BeegNumber.java
+++ src/main/java/io/acari/core/BeegNumber.java
@@ -1,4 +1,16 @@
 package io.acari.core;
 
 public class BeegNumber {
+
+  /**
+   * Given an integer n, return the largest number that contains
+   * exactly n digits.
+   *
+   * @param n
+   * @return
+   */
+  int largestNumber(int n) {
+    return n;
+  }
+
 }
