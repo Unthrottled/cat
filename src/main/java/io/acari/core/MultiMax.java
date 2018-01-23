6b93e70e7d409e4defa467e1927c59c9cc586f7e
DATBOI <alex@cyclicreference.space>
2018-01-23 11:51:07 -0600

    API def

--- src/main/java/io/acari/core/MultiMax.java
+++ src/main/java/io/acari/core/MultiMax.java
@@ -1,4 +1,23 @@
 package io.acari.core;
 
 public class MultiMax {
+
+  /**
+   * Given a divisor and a bound, find the largest integer N such that:
+   * <p>
+   * N is divisible by divisor.
+   * <p>
+   * N is less than or equal to bound.
+   * <p>
+   * N is greater than 0.
+   * <p>
+   * It is guaranteed that such a number exists.
+   *
+   * @param divisor
+   * @param bound
+   * @return
+   */
+  int maxMultiple(int divisor, int bound) {
+    return 0;
+  }
 }
