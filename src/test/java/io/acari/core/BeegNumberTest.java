2e239cbe40d6002be0e337b5b78de483e774176e
DATBOI <alex@cyclicreference.space>
2018-01-23 11:27:13 -0600

    Tests r kewl

---
+++ src/test/java/io/acari/core/BeegNumberTest.java
@@ -0,0 +1,22 @@
+package io.acari.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class BeegNumberTest {
+  private final BeegNumber beegNumber = new BeegNumber();
+
+  @Test
+  public void largestNumberOne() {
+    assertEquals(99, beegNumber.largestNumber(2));
+  }
+
+  @Test
+  public void largestNumberTwo() {
+    assertEquals(9, beegNumber.largestNumber(1));
+  }
+}
