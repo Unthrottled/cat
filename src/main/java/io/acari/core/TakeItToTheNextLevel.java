aad53d0f330ae933874f514a88c6cc4188e48f39
DATBOI <alex@cyclicreference.space>
2018-01-24 14:15:56 -0600

    My bro, jetskies, Cabo

--- src/main/java/io/acari/core/TakeItToTheNextLevel.java
+++ src/main/java/io/acari/core/TakeItToTheNextLevel.java
@@ -1,4 +1,22 @@
 package io.acari.core;
 
 public class TakeItToTheNextLevel {
+  /**
+   * You are playing an RPG game. Currently your experience
+   * points (XP) total is equal to experience. To reach the
+   * next level your XP should be at least at threshold. If you
+   * kill the monster in front of you, you will gain more
+   * experience points in the amount of the reward.
+   * <p>
+   * Given values experience, threshold and reward,
+   * check if you reach the next level after killing the monster.
+   *
+   * @param experience
+   * @param threshold
+   * @param reward
+   * @return
+   */
+  boolean reachNextLevel(int experience, int threshold, int reward) {
+      return false;
+  }
 }
