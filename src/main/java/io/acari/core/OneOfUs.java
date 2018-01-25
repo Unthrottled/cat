7268b8b583f1b933b4b286630071e0c71972047b
DATBOI <alex@cyclicreference.space>
2018-01-25 15:53:12 -0600

    2 ez

--- src/main/java/io/acari/core/OneOfUs.java
+++ src/main/java/io/acari/core/OneOfUs.java
@@ -13,6 +13,11 @@ public class OneOfUs {
    * @return
    */
   int extraNumber(int a, int b, int c) {
-    return a;
+    if(a == b)
+      return c;
+    else if(a == c)
+      return b;
+    else
+      return a;
   }
 }
