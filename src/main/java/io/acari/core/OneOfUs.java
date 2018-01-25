6a54ae6d5662d4669ef12b8e5ba22f73fc8adada
DATBOI <alex@cyclicreference.space>
2018-01-25 15:42:42 -0600

    api def

--- src/main/java/io/acari/core/OneOfUs.java
+++ src/main/java/io/acari/core/OneOfUs.java
@@ -1,4 +1,18 @@
 package io.acari.core;
 
 public class OneOfUs {
+
+  /**
+   * You're given three integers, a, b and c. It is guaranteed
+   * that two of these integers are equal to each other.
+   * What is the value of the third integer?
+   *
+   * @param a
+   * @param b
+   * @param c
+   * @return
+   */
+  int extraNumber(int a, int b, int c) {
+    return a;
+  }
 }
