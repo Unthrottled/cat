762de20f7952a9c00171b6a75acb5635c6b5be7a
DATBOI <alex@cyclicreference.space>
2018-01-26 14:06:29 -0600

    api deffo

--- src/main/java/io/acari/core/DennisTheTennis.java
+++ src/main/java/io/acari/core/DennisTheTennis.java
@@ -1,4 +1,21 @@
 package io.acari.core;
 
 public class DennisTheTennis {
+
+  /**
+   * In tennis, a set is finished when one of the players wins 6 games and the other one
+   * wins less than 5, or, if both players win at least 5 games, until one of
+   * the players wins 7 games.
+   * <p>
+   * Determine if it is possible for a tennis set to be
+   * finished with the score score1 : score2.
+   *
+   * @param score1
+   * @param score2
+   * @return
+   */
+  boolean tennisSet(int score1, int score2) {
+    return false;
+  }
+
 }
