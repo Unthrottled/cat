a3046110ff77bcbc764e06f1003899b3955e2190
DATBOI <alex@cyclicreference.space>
2018-01-26 13:16:32 -0600

    dat api def

--- src/main/java/io/acari/core/InfiniteProc.java
+++ src/main/java/io/acari/core/InfiniteProc.java
@@ -1,4 +1,21 @@
 package io.acari.core;
 
 public class InfiniteProc {
+
+  /**
+   * Given integers a and b, determine whether the following pseudocode results in an infinite loop
+   * <p>
+   * while a is not equal to b do
+   * increase a by 1
+   * decrease b by 1
+   * <p>
+   * Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+   *
+   * @param a
+   * @param b
+   * @return
+   */
+  boolean isInfiniteProcess(int a, int b) {
+    return false;
+  }
 }
