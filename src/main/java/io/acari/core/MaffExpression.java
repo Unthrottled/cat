828c62762381213c4185c8c3c854289a73152fe1
DATBOI <alex@cyclicreference.space>
2018-01-26 13:57:37 -0600

    Dat Def

--- src/main/java/io/acari/core/MaffExpression.java
+++ src/main/java/io/acari/core/MaffExpression.java
@@ -1,4 +1,19 @@
 package io.acari.core;
 
 public class MaffExpression {
+
+  /**
+   * Consider an arithmetic expression of the form a#b=c.
+   * Check whether it is possible to replace # with one of
+   * the four signs: +, -, * or / to obtain a correct expression.
+   *
+   * @param a
+   * @param b
+   * @param c
+   * @return
+   */
+  boolean arithmeticExpression(int a, int b, int c) {
+    return false;
+  }
+
 }
