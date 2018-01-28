c5aedca8ca77666497f6bd0b151e3378f5d063ed
DATBOI <alexsimons9999@gmail.com>
2018-01-27 20:18:48 -0600

    Da Wae

--- src/main/java/io/acari/core/Packen.java
+++ src/main/java/io/acari/core/Packen.java
@@ -1,4 +1,32 @@
 package io.acari.core;
 
 public class Packen {
+
+    /**
+     * You are given an array of up to four
+     * non-negative integers, each less than 256.
+     * <p>
+     * Your task is to pack these integers
+     * into one number M in the following way:
+     * <p>
+     * The first element of the array
+     * occupies the first 8 bits of M;
+     * The second element occupies next
+     * 8 bits, and so on.
+     * <p>
+     * Return the obtained integer M.
+     * <p>
+     * Note: the phrase "first bits of M"
+     * refers to the least significant bits
+     * of M - the right-most bits of an integer.
+     * For further clarification see the
+     * following example.
+     *
+     * @param a
+     * @return
+     */
+    int arrayPacking(int[] a) {
+        return 0;
+    }
+
 }
