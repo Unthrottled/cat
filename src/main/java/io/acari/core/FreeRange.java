42b2075adbb32b48e71ea7b7361676496b97c426
DATBOI <alex@cyclicreference.space>
2018-01-28 16:30:13 -0600

    API def

--- src/main/java/io/acari/core/FreeRange.java
+++ src/main/java/io/acari/core/FreeRange.java
@@ -1,4 +1,19 @@
 package io.acari.core;
 
 public class FreeRange {
+  /**
+   * You are given two numbers a and b where 0 ≤ a ≤ b.
+   * Imagine you construct an array of all the integers
+   * from a to b inclusive. You need to count the number of
+   * 1s in the binary representations of all the numbers
+   * in the array.
+   *
+   * @param a
+   * @param b
+   * @return
+   */
+  int rangeBitCount(int a, int b) {
+    return a;
+  }
+
 }
