bbb73f874096b9da1d936395a910557331b6a3c8
DATBOI <alex@cyclicreference.space>
2018-01-28 16:39:11 -0600

    API def

--- src/main/java/io/acari/core/MirroredBits.java
+++ src/main/java/io/acari/core/MirroredBits.java
@@ -1,4 +1,15 @@
 package io.acari.core;
 
 public class MirroredBits {
+
+  /**
+   * Reverse the order of the bits in a given integer.
+   *
+   * @param a
+   * @return
+   */
+  int mirrorBits(int a) {
+    return a;
+  }
+
 }
