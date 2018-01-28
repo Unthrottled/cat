eff97286c1aeb4b4dff421b3f97d1ff7035102d7
DATBOI <alex@cyclicreference.space>
2018-01-28 17:16:16 -0600

    api def

--- src/main/java/io/acari/core/ToTheRight.java
+++ src/main/java/io/acari/core/ToTheRight.java
@@ -1,4 +1,19 @@
 package io.acari.core;
 
 public class ToTheRight {
+
+  /**
+   * Presented with the integer n,
+   * find the 0-based position of the
+   * second rightmost zero bit in its binary
+   * representation (it is guaranteed that such a bit exists),
+   * counting from right to left.
+   *
+   * @param n
+   * @return
+   */
+  int secondRightmostZeroBit(int n) {
+    return 0;
+  }
+
 }
