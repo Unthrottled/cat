85a77332176b1fd24dee79a6c2f47ee0953bdd26
DATBOI <alex@cyclicreference.space>
2018-01-29 16:18:40 -0600

    API DEFFO

--- src/main/java/io/acari/core/SmallestFactorial.java
+++ src/main/java/io/acari/core/SmallestFactorial.java
@@ -2,4 +2,19 @@ package io.acari.core;
 
 public class SmallestFactorial {
 
+  /**
+   * Given an integer n, find the minimal k such that
+   * <p>
+   * k = m! (where m! = 1 * 2 * ... * m) for some integer m;
+   * k >= n.
+   * <p>
+   * In other words, find the smallest factorial which is not less than n.
+   *
+   * @param n
+   * @return
+   */
+  int leastFactorial(int n) {
+    return n;
+  }
+
 }
