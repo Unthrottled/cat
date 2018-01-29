a8eca668027ee8d7899d438f89d123f30e8ad1d1
DATBOI <alex@cyclicreference.space>
2018-01-29 16:15:04 -0600

    finally dun

--- src/main/java/io/acari/core/BitLivesMatter.java
+++ src/main/java/io/acari/core/BitLivesMatter.java
@@ -1,5 +1,8 @@
 package io.acari.core;
 
+import java.util.function.BinaryOperator;
+import java.util.stream.IntStream;
+
 public class BitLivesMatter {
 
   /**
@@ -13,7 +16,25 @@ public class BitLivesMatter {
    * @return
    */
   int equalPairOfBits(int n, int m) {
-    return 0;
+    return ((BinaryOperator<Integer>)(a, b)->{
+      String one = Integer.toBinaryString(a);
+      String two = Integer.toBinaryString(b);
+      //FUCK YO SHIT, THIS IS STUPID
+      int max = Math.max(one.length(), two.length());
+      if(max != one.length())
+        one = IntStream.range(0, max-one.length())
+            .mapToObj(aq->"0")
+            .reduce("",(r,c)->r+c) + one;
+      if(max != two.length())
+        two = IntStream.range(0, max-two.length())
+            .mapToObj(aq->"0")
+            .reduce("",(r,c)->r+c) + two;
+      for (int i = max - 1, j = 0; i > -1; --i, j++) {
+        if(one.charAt(i) == two.charAt(i))
+          return (int)Math.pow(2,j);
+      }
+      return 0;
+    }).apply(n,m);
   }
 
 }
