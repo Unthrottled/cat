a88bd9c2e94724c94516e1b67962365856c5e35e
DATBOI <alex@cyclicreference.space>
2018-01-29 15:48:52 -0600

    API def

--- src/main/java/io/acari/core/DiffRightMostBit.java
+++ src/main/java/io/acari/core/DiffRightMostBit.java
@@ -1,4 +1,18 @@
 package io.acari.core;
 
 public class DiffRightMostBit {
+
+  /**
+   * You're given two integers, n and m. Find position of the
+   * rightmost bit in which they differ in their binary
+   * representations (it is guaranteed that such a bit exists),
+   * counting from right to left.
+   *
+   * @param n
+   * @param m
+   * @return
+   */
+  int differentRightmostBit(int n, int m) {
+    return m;
+  }
 }
