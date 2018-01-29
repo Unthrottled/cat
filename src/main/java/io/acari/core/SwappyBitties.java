1c10e27e688fb3f035e1cfc740f16018093e2f43
DATBOI <alex@cyclicreference.space>
2018-01-29 13:41:55 -0600

    API deffy

--- src/main/java/io/acari/core/SwappyBitties.java
+++ src/main/java/io/acari/core/SwappyBitties.java
@@ -1,4 +1,19 @@
 package io.acari.core;
 
 public class SwappyBitties {
+
+  /**
+   * You're given an arbitrary 32-bit integer n.
+   * Take its binary representation, split bits into it
+   * in pairs (bit number 0 and 1, bit number 2 and 3, etc.)
+   * and swap bits in each pair. Then return the
+   * result as a decimal number.
+   *
+   * @param n
+   * @return
+   */
+  int swapAdjacentBits(int n) {
+    return n;
+  }
+
 }
