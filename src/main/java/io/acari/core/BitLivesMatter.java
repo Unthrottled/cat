4d865ca99d8b49304b2b952631f6ee7cb27631cb
DATBOI <alex@cyclicreference.space>
2018-01-29 16:10:55 -0600

    api def

--- src/main/java/io/acari/core/BitLivesMatter.java
+++ src/main/java/io/acari/core/BitLivesMatter.java
@@ -2,5 +2,18 @@ package io.acari.core;
 
 public class BitLivesMatter {
 
+  /**
+   * You're given two integers, n and m. Find position of the rightmost pair of
+   * equal bits in their binary representations
+   * (it is guaranteed that such a pair exists),
+   * counting from right to left.
+   *
+   * @param n
+   * @param m
+   * @return
+   */
+  int equalPairOfBits(int n, int m) {
+    return 0;
+  }
 
 }
