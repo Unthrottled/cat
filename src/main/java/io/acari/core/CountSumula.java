5848d1fa867d89fe5cc9613e3500ea4c78299af0
DATBOI <alex@cyclicreference.space>
2018-01-29 16:33:56 -0600

    api deffo, 4 real

--- src/main/java/io/acari/core/CountSumula.java
+++ src/main/java/io/acari/core/CountSumula.java
@@ -1,4 +1,19 @@
 package io.acari.core;
 
 public class CountSumula {
+
+  /**
+   * Given integers n, l and r,
+   * find the number of ways to represent n as
+   * a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
+   *
+   * @param n
+   * @param l
+   * @param r
+   * @return
+   */
+  int countSumOfTwoRepresentations2(int n, int l, int r) {
+    return r;
+  }
+
 }
