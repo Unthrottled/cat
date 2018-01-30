17e3da62b30c3b000bcaa756ae488d0e106fd899
DATBOI <alex@cyclicreference.space>
2018-01-30 15:24:06 -0600

    API def

--- src/main/java/io/acari/core/WellWellWell.java
+++ src/main/java/io/acari/core/WellWellWell.java
@@ -1,4 +1,19 @@
 package io.acari.core;
 
 public class WellWellWell {
+
+  /**
+   * You are standing at a magical well. It has two positive integers
+   * written on it: a and b. Each time you cast a magic marble into
+   * the well, it gives you a * b dollars and then both a and b
+   * increase by 1. You have n magic marbles. How much money will you make?
+   *
+   * @param a
+   * @param b
+   * @param n
+   * @return
+   */
+  int magicalWell(int a, int b, int n) {
+    return a;
+  }
 }
