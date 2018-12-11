3242aaef9ad51bd3d76f4ef11b3ad35d04ceafec
DATBOI <alexsimons9999@gmail.com>
2018-12-10 18:45:58 -0600

    I was told there would be cake

--- src/main/java/io/acari/intro/BDaCandles.kt
+++ src/main/java/io/acari/intro/BDaCandles.kt
@@ -1,2 +1,26 @@
 package io.acari.intro
 
+import java.lang.RuntimeException
+import java.util.*
+import java.util.stream.Collectors
+
+fun birthdayCakeCandles(candleHeights: List<Int>): Int {
+  return candleHeights.stream().
+      collect(Collectors.toMap({ a: Int-> a}, {1}, { a, b -> a+ b}))
+      .entries.stream()
+      .max{ a, b->a.key - b.key }
+      .map { it.value }
+      .orElseThrow { RuntimeException("I was told there was cake") }
+}
+
+fun main(args: Array<String>) {
+  val scan = Scanner(System.`in`)
+
+  val arCount = scan.nextLine().trim().toInt()
+
+  val ar = scan.nextLine().split(" ").map { it.trim().toInt() }.toList()
+
+  val result = birthdayCakeCandles(ar)
+
+  println(result)
+}
