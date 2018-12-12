e167a701088e6a479bca7f00a752bee276fe580e
DATBOI <alexsimons9999@gmail.com>
2018-12-11 18:44:54 -0600

    So I have the sums, now what?

--- src/main/java/io/acari/intro/MagicSquares.kt
+++ src/main/java/io/acari/intro/MagicSquares.kt
@@ -1,8 +1,28 @@
 package io.acari.intro
 
 import java.util.*
+import java.util.stream.Collectors
 
 fun formingMagicSquare(s: Array<Array<Int>>): Int {
+  val sums = listOf<(Array<Array<Int>>) -> List<Int>>(
+      {
+        it.map { column -> column.sum() }
+      },
+      {
+        0.until(s.size).map { row ->
+          0.until(s.size).map { col -> s[row][col] }.sum()
+        }
+      },
+      {
+        listOf(
+            0.until(s.size).map { row -> s[row][row] }.sum(),
+            (s.size - 1).downTo(0)
+                .map { row -> s[(s.size - 1) - row][row] }.sum()
+        )
+      }
+  ).flatMap { it(s) }
+      .stream().collect(Collectors.groupingBy { i: Int -> i })
+
   return 0
 }
 
@@ -12,7 +32,7 @@ fun main(args: Array<String>) {
   val s = Array<Array<Int>>(3, { Array<Int>(3, { 0 }) })
 
   for (i in 0 until 3) {
-    s[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    s[i] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
   }
 
   val result = formingMagicSquare(s)
