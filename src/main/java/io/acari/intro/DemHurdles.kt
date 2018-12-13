3fd11c9aa2bbf67f6f037214e1b9f1d7167986d1
DATBOI <alexsimons9999@gmail.com>
2018-12-12 18:11:17 -0600

    Ok, 5am, it works.  For real.

--- src/main/java/io/acari/intro/DemHurdles.kt
+++ src/main/java/io/acari/intro/DemHurdles.kt
@@ -1,2 +1,22 @@
 package io.acari.intro
 
+import java.util.*
+
+fun hurdleRace(k: Int, height: Array<Int>): Int =
+    k
+
+fun main(args: Array<String>) {
+  val scan = Scanner(System.`in`)
+
+  val nk = scan.nextLine().split(" ")
+
+  val n = nk[0].trim().toInt()
+
+  val k = nk[1].trim().toInt()
+
+  val height = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+  val result = hurdleRace(k, height)
+
+  println(result)
+}
