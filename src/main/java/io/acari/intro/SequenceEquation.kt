488bf1f9a7166c8c1b25c4ffe30457b2a15cfd45
DATBOI <alexsimons9999@gmail.com>
2018-12-12 18:20:48 -0600

    Some bugs fixed

--- src/main/java/io/acari/intro/SequenceEquation.kt
+++ src/main/java/io/acari/intro/SequenceEquation.kt
@@ -1,2 +1,19 @@
 package io.acari.intro
 
+import java.util.*
+
+fun permutationEquation(p: Array<Int>): Array<Int> {
+  return p
+}
+
+fun main(args: Array<String>) {
+  val scan = Scanner(System.`in`)
+
+  val n = scan.nextLine().trim().toInt()
+
+  val p = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+  val result = permutationEquation(p)
+
+  println(result.joinToString("\n"))
+}
