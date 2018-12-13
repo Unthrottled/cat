50e605b1414b04d932840fdfc8b96bd6e4b532ef
DATBOI <alexsimons9999@gmail.com>
2018-12-12 18:42:32 -0600

    (｡◕‿‿◕｡)

--- src/main/java/io/acari/intro/SequenceEquation.kt
+++ src/main/java/io/acari/intro/SequenceEquation.kt
@@ -1,9 +1,16 @@
 package io.acari.intro
 
 import java.util.*
+import java.util.stream.Collectors
 
-fun permutationEquation(p: Array<Int>): Array<Int> {
-  return p
+fun permutationEquation(p: Array<Int>): List<Int> {
+  val copy = p.mapIndexed { index, i -> Pair(index+1, i) }.stream()
+      .collect(Collectors.toMap(
+      { pair: Pair<Int, Int> -> pair.second },
+      { pair -> pair.first },
+      { a, _ -> a }))
+
+  return 1.rangeTo(p.size).map { copy[copy[it]] ?: 0 }
 }
 
 fun main(args: Array<String>) {
@@ -11,7 +18,7 @@ fun main(args: Array<String>) {
 
   val n = scan.nextLine().trim().toInt()
 
-  val p = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+  val p = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
 
   val result = permutationEquation(p)
 
